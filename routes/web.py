e091017c52ce9d2ce50f0fdfc2b3f961874731c1
Joseph Mancuso <idmann509@gmail.com>
2017-12-06 09:33:26 -0500

    linted web routes

--- routes/web.py
+++ routes/web.py
@@ -1,6 +1,5 @@
+''' Web Routes '''
 from app.http.providers.routes import Get
-from resources.views.home import Home
-from app.http.providers.view import view
 from app.http.controllers.WelcomeController import WelcomeController
 
 routes = [

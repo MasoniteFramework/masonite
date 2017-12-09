d9b8818e5a68f3c0b278f64c4d8abbe9989e2d85
Joseph Mancuso <idmann509@gmail.com>
2017-12-09 13:29:49 -0500

    added Post class to web.py

--- routes/web.py
+++ routes/web.py
@@ -1,5 +1,5 @@
 ''' Web Routes '''
-from app.http.providers.routes import Get
+from app.http.providers.routes import Get, Post
 from app.http.controllers.WelcomeController import WelcomeController
 
 routes = [

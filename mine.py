dc8eaa6b679cf119474a0ab642649f1f89fce136
Joseph Mancuso <idmann509@gmail.com>
2017-12-15 14:43:41 -0500

    fixed whitenoise not generating asset

--- mine.py
+++ mine.py
@@ -4,3 +4,4 @@ from whitenoise import WhiteNoise
 from app.start import app
 
 application = WhiteNoise(app, root='storage/static')
+application.add_files('storage/static', prefix='storage/static/')

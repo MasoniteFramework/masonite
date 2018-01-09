9a23503cda1638ebf91f63c3777c81c70a6a1219
Joseph Mancuso <idmann509@gmail.com>
2018-01-09 13:56:14 -0500

    made default auth system support only Orator ORM

--- masonite/facades/Auth.py
+++ masonite/facades/Auth.py
@@ -13,29 +13,29 @@ class Auth(object):
         ''' Returns the model specified in the auth.py configuration '''
         try:
             if self.request.get_cookie('token'):
-                return auth.AUTH['model'].get(
-                    auth.AUTH['model'].token == self.request.get_cookie('token')
-                )
+                return auth.AUTH['model'].where(
+                    'remember_token', self.request.get_cookie('token')
+                ).first()
             
             return False
         except Exception as exception:
-            pass
+            raise exception
 
-        return False
+        return None
 
     def login(self, name, password):
         ''' Login the user based on the parameters provided '''
-        auth_column = getattr(auth.AUTH['model'], auth.AUTH['model']._meta.auth_column)
+        auth_column = auth.AUTH['model'].__auth__
         try:
-            model = auth.AUTH['model'].get(
-                auth_column == name,
-            )
-            if model.password.check_password(password):
-                token = uuid.uuid4()
-                model.token = token
+            model = auth.AUTH['model'].where(auth_column, name).where('password', password).first()
+            
+            if model:
+                remember_token = str(uuid.uuid4())
+                model.remember_token = remember_token
                 model.save()
-                self.request.cookie('token', token)
+                self.request.cookie('token', remember_token)
                 return model
+                
         except Exception as exception:
             raise exception
 

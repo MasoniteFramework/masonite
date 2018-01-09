0ad6670be1cb240e733dcd5fb83647472d21718c
Joseph Mancuso <idmann509@gmail.com>
2018-01-08 21:51:57 -0500

    removed reference of peewee from database config. First step in phasing peewee out

--- config/database.py
+++ config/database.py
@@ -2,16 +2,17 @@
 import os
 
 from dotenv import find_dotenv, load_dotenv
-from peewee import MySQLDatabase
+from orator import DatabaseManager, Model
 
 load_dotenv(find_dotenv())
 
-DRIVER = 'mysql'
-
 ENGINES = {
-    'default': MySQLDatabase(
-        os.environ.get('DB_DATABASE'),
-        user=os.environ.get('DB_USERNAME'),
-        password=os.environ.get('DB_PASSWORD'),
-        host=os.environ.get('DB_HOST'))
+    'mysql': {
+        'driver': 'mysql',
+        'host': 'localhost',
+        'database': 'masonite',
+        'user': 'root',
+        'password': '',
+        'prefix': ''
+    }
 }

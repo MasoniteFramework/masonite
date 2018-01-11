28bebffcdc5eea73d24cdfa6effbaf3463e016a4
Afdol Rizki <afdolriski@gmail.com>
2018-01-11 13:50:03 +0700

    put try catch block when import sass module

--- masonite/storage.py
+++ masonite/storage.py
@@ -1,6 +1,5 @@
 from config import storage, application
 import os
-import sass
 
 class Storage(object):
 
@@ -8,20 +7,25 @@ class Storage(object):
         pass
 
     def compile_sass(self):
-        matches = []
-        for files in storage.SASSFILES['importFrom']:
-            for root, dirnames, filenames in os.walk(os.path.join(application.BASE_DIRECTORY, files)):
-                for filename in filenames:
-                    if filename.endswith(('.sass', '.scss')) and not filename.startswith('_'):
-                        matches.append(os.path.join(root, filename))
+        try:
+            import sass
+        except ImportError as e:
+            pass
+        else:
+            matches = []
+            for files in storage.SASSFILES['importFrom']:
+                for root, dirnames, filenames in os.walk(os.path.join(application.BASE_DIRECTORY, files)):
+                    for filename in filenames:
+                        if filename.endswith(('.sass', '.scss')) and not filename.startswith('_'):
+                            matches.append(os.path.join(root, filename))
 
-        for filename in matches:
-            with open(filename) as f:
-                compiled_sass = sass.compile(
-                    string=f.read(), include_paths=storage.SASSFILES['includePaths']
-                )
-                name = filename.split(os.sep)[-1].replace('.scss', '').replace('.sass', '')
-                write_file = os.path.join(os.path.join(application.BASE_DIRECTORY, 
-                    storage.SASSFILES['compileTo']), '{0}.css'.format(name))
-            with open(write_file, 'w') as r:
-                r.write(compiled_sass)
+            for filename in matches:
+                with open(filename) as f:
+                    compiled_sass = sass.compile(
+                        string=f.read(), include_paths=storage.SASSFILES['includePaths']
+                    )
+                    name = filename.split(os.sep)[-1].replace('.scss', '').replace('.sass', '')
+                    write_file = os.path.join(os.path.join(application.BASE_DIRECTORY, 
+                        storage.SASSFILES['compileTo']), '{0}.css'.format(name))
+                with open(write_file, 'w') as r:
+                    r.write(compiled_sass)

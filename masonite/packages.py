545f4509d30a981e27db543597b883a91deee147
Joseph Mancuso <idmann509@gmail.com>
2018-01-11 21:13:50 -0500

    added 3 new functions for package support

--- masonite/packages.py
+++ masonite/packages.py
@@ -4,8 +4,6 @@ import sys
 import shutil
 
 def create_or_append_config(location, name=False):
-    module_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-
     if name:
         file_name = name
     else:
@@ -30,3 +28,53 @@ def create_or_append_config(location, name=False):
         project_config.close()
         package_config.close()
         print('\033[92mConfiguration File Appended!\033[0m')
+
+
+def append_web_routes(location):
+    # import it into the web.py file
+    routes_file = os.path.join(os.getcwd(), 'routes/web.py')
+
+    ## Append to the file
+    # tempfiles is a list of file handles to your temp files. Order them however you like
+    project_routes = open(routes_file, "a")
+    package_routes = open(location, 'r')
+
+    project_routes.write(package_routes.read())
+
+    project_routes.close()
+    package_routes.close()
+    print('\033[92mroutes/web.py File Appended!\033[0m')
+
+def append_api_routes(location):
+    # import it into the web.py file
+    api_file = os.path.join(os.getcwd(), 'routes/api.py')
+
+    ## Append to the file
+    # tempfiles is a list of file handles to your temp files. Order them however you like
+    project_routes = open(api_file, "a")
+    package_routes = open(location, 'r')
+
+    project_routes.write(package_routes.read())
+
+    project_routes.close()
+    package_routes.close()
+    print('\033[92mroutes/api.py File Appended!\033[0m')
+
+
+def create_controller(location):
+    file_name = os.path.basename(location)
+
+    controller_directory = os.path.join(os.getcwd(), 'app/http/controllers')
+    controller_file = os.path.join(controller_directory, file_name)
+
+    if os.path.isfile(controller_file):
+        # if file does exist
+        print('\033[91m{0} Controller Already Exists!\033[0m'.format(file_name))
+    else:
+        # copy controller over
+        shutil.copyfile(
+            location,
+            controller_file
+        )
+
+        print('\033[92m{0} Controller Created\033[0m'.format(file_name))

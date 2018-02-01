d6519c47a8e5c4e7b587a9471a986ed28143018b
Joseph Mancuso <idmann509@gmail.com>
2018-02-01 09:15:26 -0500

    added controller base class to masonite

---
+++ tests/test_controllers.py
@@ -0,0 +1,9 @@
+from masonite.app import App
+from masonite.controllers import Controller
+
+def test_controller_loads_app():
+    app = App()
+    app.bind('object', object)
+
+    controller = Controller().load_app(app)
+    assert controller.app.providers == {'object': object}

25d588b05643f6f8b060557c0c5d1b84a1254852
Joseph Mancuso <idmann509@gmail.com>
2018-02-01 23:55:24 -0500

    added more flagpole comments to bootstrap/start.py

--- bootstrap/start.py
+++ bootstrap/start.py
@@ -11,49 +11,83 @@ from masonite.app import App
 from config import application
 from pydoc import locate
 
+'''
+|--------------------------------------------------------------------------
+| Compile Sass
+|--------------------------------------------------------------------------
+|
+| Compile Sass if the libsass module is installed. Once installed, all
+| Sass files are compiled when the server is ran. This will only run
+| once when the server is first started.
+|
+'''
 
-# Run once and only once the server is ran
-# This will not actually compile if the libsass module in not installed
 Storage().compile_sass()
 
 def app(environ, start_response):
     ''' Framework Engine '''
-    # Instantiate the Service Container
-    app = App()
+    
+    '''
+    |--------------------------------------------------------------------------
+    | Startup the Service Container
+    |--------------------------------------------------------------------------
+    |
+    | Instantiate the Service Container so we can bind classes into it and 
+    | bind the environ variable that is created by the WSGI server into
+    | the container.
+    |
+    '''
 
+    app = App()
     app.bind('Environ', environ)
     
-    # Execute all the register methods in all containers
+    '''
+    |--------------------------------------------------------------------------
+    | Execute All Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | Run all service providers register and boot methods to load objects into
+    | the container. Ensure that the providers register methods are executed
+    | first, and then run through the list and execute the boot methods.
+    | This ensures that the boot method has access to methods in the
+    | container.
+    |
+    '''
+
     for provider in application.PROVIDERS:
         locate(provider)().load_app(app).register()
 
-    # Execute all the boot methods in all containers
     for provider in application.PROVIDERS:
         app.resolve(locate(provider)().load_app(app).boot)
-    
-    request = app.make('Request')
 
+    request = app.make('Request')
     router = app.make('Route')
 
-    if router.is_post():
-        # Set POST parameters given into the query string to centralize parsing and retrieval.
-        # This was made to directly support the Request.input() method.
-        # There is no known reason to differentiate between GET and POST when retrieving
-        #    form parameters. This line below simply puts both POST form params in the
-        #    same query_string as GET params.
-        environ['QUERY_STRING'] = router.set_post_params()
+    '''
+    |--------------------------------------------------------------------------
+    | Run Through All Http Routes
+    |--------------------------------------------------------------------------
+    |
+    | Retrieve and loop through the web routes from the Service Container.
+    |
+    '''
 
-    # Check all http routes
     for route in app.make('WebRoutes'):
         # Compiles the given route to regex
         regex = router.compile_route_to_regex(route)
 
-        # Make a better match trailing slashes `/` at the end of routes
-        # Sometimes a user will end with a trailing slash. Because the
-        #    user may create routes like `/url/route` and `/url/route/`
-        #    and how the regex is compiled down, we may need to adjust for
-        #    urls that end or dont end with a trailing slash. This is sort
-        #    of a quirk and could possibly be fixed.
+        '''
+        |--------------------------------------------------------------------------
+        | Make a better match for trailing slashes
+        |--------------------------------------------------------------------------
+        |
+        | Sometimes a user will end with a trailing slash. Because the user might
+        | create routes like `/url/route` and `/url/route/` and how the regex 
+        | is compiled down, we may need to adjust for urls that end or dont 
+        | end with a trailing slash.
+        |
+        '''
+        
         if route.route_url.endswith('/'):
             matchurl = re.compile(regex.replace(r'\/\/$', r'\/$'))
         else:
@@ -70,11 +104,28 @@ def app(environ, start_response):
         except AttributeError:
             pass
 
-        # If the route url matches the regex and the method type and the route can continue
-        #     (will be used with middleware later)
+        '''
+        |--------------------------------------------------------------------------
+        | Houston, we've got a match
+        |--------------------------------------------------------------------------
+        |
+        | Check to see if a route matches the corresponding router url. If a match
+        | is found, execute that route and break out of the loop. We only need
+        | one match. Routes are executed on a first come, first serve basis
+        |
+        '''
+
         if matchurl.match(router.url) and route.method_type == environ['REQUEST_METHOD']:
 
-            # Execute HTTP Before Middleware
+            '''
+            |--------------------------------------------------------------------------
+            | Execute Before Middleware
+            |--------------------------------------------------------------------------
+            |
+            | This is middleware that contains a before method.
+            |
+            '''
+
             for http_middleware in app.make('HttpMiddleware'):
                 located_middleware = locate(http_middleware)
                 if hasattr(located_middleware, 'before'):
@@ -96,7 +147,15 @@ def app(environ, start_response):
             #     request object. This is after middleware and is ran after the request
             route.load_request(request).run_middleware('after')
 
-            # Execute HTTP After Middleware
+            '''
+            |--------------------------------------------------------------------------
+            | Execute After Middleware
+            |--------------------------------------------------------------------------
+            |
+            | This is middleware with an after method.
+            |
+            '''
+
             for http_middleware in app.make('HttpMiddleware'):
                 located_middleware = locate(http_middleware)
                 if hasattr(located_middleware, 'after'):
@@ -105,14 +164,39 @@ def app(environ, start_response):
         else:
             data = 'Route not found. Error 404'
 
-    ## Check all API Routes
+    '''
+    |--------------------------------------------------------------------------
+    | No Matches for Http Routes Were Found
+    |--------------------------------------------------------------------------
+    |
+    | Since, we found no matches, let's look through the Api routes
+    |
+    '''
+
     if data == 'Route not found. Error 404':
-        # Look at the API routes files
-        
+
+        '''
+        |--------------------------------------------------------------------------
+        | Pull in the API Routes from the Service Container
+        |--------------------------------------------------------------------------
+        |
+        | The Service Container has loaded all Api routes into the container so
+        | let's loop through and check for any matches.
+        |
+        '''
 
         for route in app.make('ApiRoutes'):
 
-            # If the base url matches
+            '''
+            |--------------------------------------------------------------------------
+            | If We've Got A Match
+            |--------------------------------------------------------------------------
+            |
+            | If we have a match then let's go ahead and execute the route, load the 
+            | response into the data variable and get on with our lives.
+            |
+            '''
+
             if route.url in router.url:
                 data = route.fetch(request).output
                 if data:
@@ -122,9 +206,16 @@ def app(environ, start_response):
             else:
                 data = 'Route not found. Error 404'
 
-    # Set redirection if a route has been called to redirect to.
-    # redirect_route is a property set on the request object when
-    #     methods like request.redirectTo('nameRoute') are called
+    '''
+    |--------------------------------------------------------------------------
+    | Redirect To A Named Route
+    |--------------------------------------------------------------------------
+    |
+    | Sometimes we may need to redirect to a named route. Because of this, we
+    | will need to loop back through all the routes and find a route with a
+    | name that matches the named route we supplied.
+    |
+    '''
     if request.redirect_route:
         for route in routes:
             if route.named_route == request.redirect_route:
@@ -132,6 +223,17 @@ def app(environ, start_response):
                 data = router.get(route.route, route.output(request))
                 request.redirect_url = route.route_url
 
+    '''
+    |--------------------------------------------------------------------------
+    | We Are Ready For Launch
+    |--------------------------------------------------------------------------
+    |
+    | If we have a solid response and not redirecting then we need to return
+    | a 200 status code along with the data. If we don't, then we'll have
+    | to return a 302 and redirect to where the developer would like to
+    | go next.
+    |
+    '''
     if not request.redirect_url:
         # Convert the data that is retrieved above to bytes so the wsgi server can handle it.
         data = bytes(data, 'utf-8')
@@ -143,9 +245,19 @@ def app(environ, start_response):
         ] + request.get_cookies())
     else:
         data = bytes('redirecting ..', 'utf-8')
+
         # Redirection. In order to redirect the response types need to be 302 instead of 200
         start_response("302 OK", [
             ('Location', request.compile_route_to_url())
         ] + request.get_cookies())
-    # This will output the data to the browser.
+
+    '''
+    |--------------------------------------------------------------------------
+    | Final Step
+    |--------------------------------------------------------------------------
+    |
+    | This will take the data variable from above and return it to the WSGI
+    | server.
+    |
+    '''
     return iter([data])

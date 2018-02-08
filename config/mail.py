be50d707f14747f0fa1d4fb2f6ec1ef98f4e1dbd
Joseph Mancuso <idmann509@gmail.com>
2018-02-08 12:20:50 -0500

    fixed comment for storage drivers

--- config/mail.py
+++ config/mail.py
@@ -31,12 +31,10 @@ DRIVER = os.getenv('MAIL_DRIVER', 'smtp')
 
 '''
 |--------------------------------------------------------------------------
-| Application Name
+| Mail Drivers
 |--------------------------------------------------------------------------
 |
-| This value is the name of your application. This value is used when the
-| framework needs to place the application's name in a notification or
-| any other location as required by the application or its packages.
+| Different drivers you can use for sending email.
 |
 '''
 

aa8378934d000b19d952fa6c828dc0934f25c67c
Joseph Mancuso <idmann509@gmail.com>
2018-02-15 21:13:14 -0500

    removed needless __init__ controller method

--- app/http/controllers/WelcomeController.py
+++ app/http/controllers/WelcomeController.py
@@ -5,9 +5,6 @@ from masonite.request import Request
 class WelcomeController(object):
     ''' Controller for welcoming the user '''
 
-    def __init__(self):
-        pass
-
     def show(self, Application, request: Request):
         ''' Show Welcome Template '''
         return view('welcome', {'app': Application})

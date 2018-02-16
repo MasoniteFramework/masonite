dbadc13f8b38cfe4b1fee7e7d576459bdee49fb1
Joseph Mancuso <idmann509@gmail.com>
2018-02-16 07:15:56 -0500

    Fixed and added controller docstrings

--- app/http/middleware/LoadUserMiddleware.py
+++ app/http/middleware/LoadUserMiddleware.py
@@ -1,19 +1,20 @@
-''' Class for the authentication middleware '''
+''' Load User Middleware'''
 from masonite.facades.Auth import Auth
 
 class LoadUserMiddleware(object):
     ''' Middleware class which loads the current user into the request '''
 
     def __init__(self, Request):
+        ''' Inject Any Dependencies From The Service Container '''
         self.request = Request
 
     def before(self):
-        ''' Register as a before middleware to be ran before the request '''
+        ''' Run This Middleware Before The Route Executes '''
         self.load_user(self.request)
         return self.request
 
     def after(self):
-        ''' Register as an after middleware to be ran after the request '''
+        ''' Run This Middleware After The Route Executes '''
         pass
 
     def load_user(self, request):

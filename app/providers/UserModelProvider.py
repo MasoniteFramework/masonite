5513358d3191021bd1984dabc66d3ddce79728f4
Joseph Mancuso <idmann509@gmail.com>
2018-02-16 07:11:08 -0500

    added docstrings to providers

--- app/providers/UserModelProvider.py
+++ app/providers/UserModelProvider.py
@@ -1,12 +1,14 @@
+''' A User Model Service Provider '''
 from masonite.provider import ServiceProvider
 from app.User import User
 
 class UserModelProvider(ServiceProvider):
     ''' Binds the User model into the Service Container '''
 
-    wsgi = False 
-    
+    wsgi = False
+
     def register(self):
+        ''' Registers The User Into The Service Container '''
         self.app.bind('User', User)
 
     def boot(self):

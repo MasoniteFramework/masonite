82b33438a0fad5a2226af09aa60087d28691f623
Joseph Mancuso <idmann509@gmail.com>
2018-02-22 23:06:31 -0500

    fixed web socket tests to only run if ably key is set

--- tests/broadcasts/test_sockets.py
+++ tests/broadcasts/test_sockets.py
@@ -4,30 +4,33 @@ from masonite.drivers.BroadcastPusherDriver import BroadcastPusherDriver
 from masonite.drivers.BroadcastAblyDriver import BroadcastAblyDriver
 from config import broadcast
 
-def test_broadcast_loads_into_container():
-    container = TestSuite().create_container().container
+import os
 
-    container.bind('BroadcastConfig', broadcast)
-    container.bind('BroadcastPusherDriver', BroadcastPusherDriver)
-    container.bind('BroadcastManager', BroadcastManager().load_container(container))
-    container.bind('Broadcast', container.make('BroadcastManager').driver('pusher'))
+if os.getenv('ABLY_SECRET'):
+    def test_broadcast_loads_into_container():
+        container = TestSuite().create_container().container
 
-    assert container.make('BroadcastManager')
-    assert container.make('Broadcast').channel('random', 'from driver') == {'message': 'from driver'}
-    assert container.make('Broadcast').channel('random', {'message': 'dictionary'}) == {'message': 'dictionary'}
-    assert container.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}) == {'message': 'dictionary'}
-    assert container.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}, 'test-event') == {'message': 'dictionary'}
+        container.bind('BroadcastConfig', broadcast)
+        container.bind('BroadcastPusherDriver', BroadcastPusherDriver)
+        container.bind('BroadcastManager', BroadcastManager().load_container(container))
+        container.bind('Broadcast', container.make('BroadcastManager').driver('pusher'))
 
-def test_broadcast_loads_into_container():
-    container = TestSuite().create_container().container
+        assert container.make('BroadcastManager')
+        assert container.make('Broadcast').channel('random', 'from driver') == {'message': 'from driver'}
+        assert container.make('Broadcast').channel('random', {'message': 'dictionary'}) == {'message': 'dictionary'}
+        assert container.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}) == {'message': 'dictionary'}
+        assert container.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}, 'test-event') == {'message': 'dictionary'}
 
-    container.bind('BroadcastConfig', broadcast)
-    container.bind('BroadcastAblyDriver', BroadcastAblyDriver)
-    container.bind('BroadcastManager', BroadcastManager().load_container(container))
-    container.bind('Broadcast', container.make('BroadcastManager').driver('ably'))
+    def test_broadcast_loads_into_container_with_ably():
+        container = TestSuite().create_container().container
 
-    assert container.make('BroadcastManager')
-    assert container.make('Broadcast').channel('test-channel', 'from driver') == 'from driver'
-    assert container.make('Broadcast').channel('test-channel', {'message': 'from driver'}) == {'message': 'from driver'}
-    assert container.make('Broadcast').channel(['channel-1', 'channel-2'], {'message': 'dictionary'}) == {'message': 'dictionary'}
-    assert container.make('Broadcast').channel(['channel-1', 'channel-2'], {'message': 'dictionary'}, 'test-event') == {'message': 'dictionary'}
+        container.bind('BroadcastConfig', broadcast)
+        container.bind('BroadcastAblyDriver', BroadcastAblyDriver)
+        container.bind('BroadcastManager', BroadcastManager().load_container(container))
+        container.bind('Broadcast', container.make('BroadcastManager').driver('ably'))
+
+        assert container.make('BroadcastManager')
+        assert container.make('Broadcast').channel('test-channel', 'from driver') == 'from driver'
+        assert container.make('Broadcast').channel('test-channel', {'message': 'from driver'}) == {'message': 'from driver'}
+        assert container.make('Broadcast').channel(['channel-1', 'channel-2'], {'message': 'dictionary'}) == {'message': 'dictionary'}
+        assert container.make('Broadcast').channel(['channel-1', 'channel-2'], {'message': 'dictionary'}, 'test-event') == {'message': 'dictionary'}

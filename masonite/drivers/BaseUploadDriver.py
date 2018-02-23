967369ec198f713b0c722dcd48437f794454737d
Martín Peveri <martinpeveri@gmail.com>
2018-02-23 15:56:51 -0300

    Add accepted file types to upload drivers #22

---
+++ masonite/drivers/BaseUploadDriver.py
@@ -0,0 +1,26 @@
+import os
+from masonite.exceptions import FileTypeException
+
+
+class BaseUploadDriver(object):
+    """
+    Class base for upload drivers
+    """
+
+    # this will accept all file types
+    accept_file_types = None
+
+    def accept(self, *args, **kwargs):
+        """
+        Set file types to accept
+        """
+        self.accept_file_types = args
+        return self
+
+    def validate_extension(self, filename):
+        """
+        Check if files is a valid extension
+        """
+        if self.accept_file_types is not None:
+            if not filename.endswith(self.accept_file_types):
+                raise FileTypeException("The extension file not is valid.")
--- tests/test_upload_manager.py
+++ tests/test_upload_manager.py
@@ -1,9 +1,11 @@
-from config import storage
+import os
 import pytest
 
+from config import application, storage
 from masonite.app import App
-from masonite.exceptions import DriverNotFound
+from masonite.exceptions import DriverNotFound, FileTypeException
 from masonite.managers.UploadManager import UploadManager
+from masonite.drivers.UploadDiskDriver import UploadDiskDriver
 
 
 def test_upload_manager_grabs_drivers():
@@ -22,7 +24,10 @@ def test_upload_manager_raises_driver_not_found_error():
     container.bind('StorageConfig', storage)
 
     with pytest.raises(DriverNotFound):
-        container.bind('UploadManager', UploadManager().load_container(container))
+        container.bind(
+            'UploadManager',
+            UploadManager().load_container(container)
+        )
 
 
 def test_upload_manager_switches_drivers():
@@ -32,5 +37,68 @@ def test_upload_manager_switches_drivers():
     container.bind('StorageConfig', storage)
     container.bind('UploadDiskDriver', object)
     container.bind('UploadTestDriver', object)
-
     container.bind('UploadManager', UploadManager(container).driver('test'))
+
+
+class ImageTest():
+    """
+    Image test for emulate upload file
+    """
+
+    @property
+    def filename(self):
+        return os.getcwd() + "/tests/static/test.jpg"
+
+    @property
+    def file(self):
+        return open(self.filename, 'rb')
+
+
+def test_upload_file():
+    """
+    This test is responsible for checking if you upload a file correctly.
+    """
+
+    container = App()
+
+    container.bind('Application', application)
+    container.bind('StorageConfig', storage)
+    container.bind('UploadDiskDriver', UploadDiskDriver)
+
+    img = ImageTest()
+    assert UploadManager(container).driver('disk').store(img)
+
+
+def test_upload_manage_accept_files():
+    """
+    This test is responsible for checking if you upload
+    a file correctly with a valid extension.
+    """
+
+    container = App()
+
+    container.bind('Application', application)
+    container.bind('StorageConfig', storage)
+    container.bind('UploadDiskDriver', UploadDiskDriver)
+
+    img = ImageTest()
+    assert UploadManager(container).driver('disk').accept('jpg', 'png').store(img)
+
+
+def test_upload_manage_accept_files_error():
+    """
+    This test should return an error because it is an invalid extension.
+    """
+
+    container = App()
+
+    container.bind('Application', application)
+    container.bind('StorageConfig', storage)
+    container.bind('UploadDiskDriver', UploadDiskDriver)
+
+    img = ImageTest()
+    try:
+        UploadManager(container).driver('disk').accept('png').store(img)
+        assert False
+    except FileTypeException:
+        assert True

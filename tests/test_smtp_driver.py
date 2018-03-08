758f7ebca4f15f6656b096abcd00958bb5852a25
Joseph Mancuso <idmann509@gmail.com>
2018-03-08 12:16:58 -0500

    fixed mailgun test in wrong file

--- tests/test_smtp_driver.py
+++ tests/test_smtp_driver.py
@@ -8,26 +8,6 @@ from masonite.drivers.MailSmtpDriver import MailSmtpDriver as MailDriver
 from masonite.drivers.MailMailgunDriver import MailMailgunDriver as Mailgun
 
 
-class User:
-    pass
-
-
-def test_mailgun_driver():
-    app = App()
-
-    app.bind('Test', object)
-    app.bind('MailConfig', mail)
-    app.bind('MailSmtpDriver', MailDriver)
-    app.bind('MailMailgunDriver', Mailgun)
-    user = User
-    user.email = 'idmann509@gmail.com'
-
-    assert MailManager(app).driver('smtp').to(
-        user).to_address == 'idmann509@gmail.com'
-    assert MailManager(app).driver('smtp').to(user).template(
-        'mail/welcome', {'to': 'MasoniteTesting'}).send() is None
-
-
 def test_mail_renders_template():
     app = App()
 

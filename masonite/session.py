6075db57833803f5df0e4037503661686eed70a6
Martín Peveri <martinpeveri@gmail.com>
2018-04-02 10:13:29 -0300

    Add Sessions #52

---
+++ masonite/session.py
@@ -0,0 +1,47 @@
+
+
+class Session():
+    """
+    Class Session for manage sessions of the app
+    """
+    _session = {}
+
+    def __init__(self, environ):
+        """
+        Constructor
+        """
+        self.environ = environ
+
+    def get(self, key):
+        """
+        Get a session from object _session
+        """
+        ip = self.__get_client_address()
+        try:
+            session = self._session[ip][key]
+        except KeyError:
+            session = ''
+
+        return session
+
+    def set(self, key, value):
+        """
+        Set a new session in object _session
+        """
+        ip = self.__get_client_address()
+        self._session[ip] = {key: value}
+
+    def reset(self):
+        """
+        Reset object _session
+        """
+        self._session = {}
+
+    def __get_client_address(self):
+        """
+        Get ip from the client
+        """
+        try:
+            return self.environ['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
+        except KeyError:
+            return self.environ['REMOTE_ADDR']

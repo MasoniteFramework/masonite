5c836a79a3a90f6c7d2b4d974da4383e3fb485cb
Joseph Mancuso <idmann509@gmail.com>
2018-04-10 13:04:02 -0400

    added session configuration file

---
+++ config/session.py
@@ -0,0 +1,16 @@
+''' Session Settings '''
+
+'''
+|--------------------------------------------------------------------------
+| Session Driver
+|--------------------------------------------------------------------------
+|
+| Sessions are able to be linked to an individual user and carry data from
+| request to request. The memory driver will store all the session data
+| inside memory which will delete when the server stops running.
+|
+| Supported: 'memory', 'cookie'
+| 
+'''
+
+DRIVER = 'memory'

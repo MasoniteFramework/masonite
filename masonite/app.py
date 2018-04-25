7119e81f23e4dea829ec3e23d436eba893c5c43d
Joseph Mancuso <idmann509@gmail.com>
2018-04-25 14:59:23 -0400

    cleaned up container class

--- masonite/app.py
+++ masonite/app.py
@@ -13,26 +13,39 @@ class App():
     def __init__(self):
         self.providers = {}
 
+
     def bind(self, name, class_obj):
-        """ Bind classes into the container with a key value pair """
+        """ 
+        Bind classes into the container with a key value pair
+        """
         self.providers.update({name: class_obj})
         return self
 
+
     def make(self, name):
-        """ Retreives a class from the container by key """
+        """
+        Retreives a class from the container by key
+        """
         if self.has(name):
             return self.providers[name]
 
         raise MissingContainerBindingNotFound("{0} key was not found in the container".format(name))
 
+
     def has(self, name):
+        """
+        Check if a key exists in the container
+        """
         if name in self.providers:
             return True
 
         return False
 
+
     def helper(self):
-        """ Adds a helper to create builtin functions """
+        """
+        Adds a helper to create builtin functions
+        """
         return self
 
 
@@ -41,7 +54,6 @@ class App():
         Takes an object such as a function or class method and resolves it's 
         parameters from objects in the container
         """
-
         provider_list = []
 
         for parameter, value in inspect.signature(obj).parameters.items():
@@ -54,16 +66,24 @@ class App():
 
 
     def _find_parameter(self, parameter):
+        """
+        Find a parameter in the container
+        """
         parameter = str(parameter)
         if parameter is not 'self' and parameter in self.providers:
             return self.providers[parameter]
         
-        raise TypeError('Could not resolve the incorrect amount of objects from the container')
+        raise TypeError(
+            'The dependency with the key of {0} could not be found in the container'.format(parameter)
+        )
 
 
     def _find_annotated_parameter(self, parameter):
+        """
+        Find a given annotation in the container
+        """
         for provider, provider_class in self.providers.items():
             if parameter.annotation == provider_class.__class__ or parameter.annotation == provider_class or isinstance(provider_class, parameter.annotation.__class__):
                 return provider_class
         
-        raise TypeError('Could not resolve the incorrect amount of objects from the container')
+        raise TypeError('The dependency with the {0} annotation could not be resolved by the container'.format(parameter))

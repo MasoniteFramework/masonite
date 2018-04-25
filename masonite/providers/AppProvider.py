a8d1ae9b89df1f7fc6238e9c892e8f39a17d65b2
Joseph Mancuso <idmann509@gmail.com>
2018-04-24 20:54:20 -0400

    added commands into app provider

--- masonite/providers/AppProvider.py
+++ masonite/providers/AppProvider.py
@@ -5,6 +5,22 @@ from masonite.routes import Route
 from config import storage
 from routes import web, api
 
+from masonite.commands.AuthCommand import AuthCommand
+from masonite.commands.CommandCommand import CommandCommand
+from masonite.commands.ControllerCommand import ControllerCommand
+from masonite.commands.InstallCommand import InstallCommand
+from masonite.commands.JobCommand import JobCommand
+from masonite.commands.KeyCommand import KeyCommand
+from masonite.commands.MakeMigrationCommand import MakeMigrationCommand
+from masonite.commands.MigrateCommand import MigrateCommand
+from masonite.commands.MigrateRefreshCommand import MigrateRefreshCommand
+from masonite.commands.MigrateResetCommand import MigrateResetCommand
+from masonite.commands.MigrateRollbackCommand import MigrateRollbackCommand
+from masonite.commands.ModelCommand import ModelCommand
+from masonite.commands.ProviderCommand import ProviderCommand
+from masonite.commands.ServeCommand import ServeCommand
+from masonite.commands.ViewCommand import ViewCommand
+
 
 class AppProvider(ServiceProvider):
 
@@ -15,6 +31,23 @@ class AppProvider(ServiceProvider):
         self.app.bind('Route', Route())
         self.app.bind('Request', Request())
         self.app.bind('Container', self.app)
+
+        # Insert Commands
+        self.app.bind('MasoniteAuthCommand', AuthCommand())
+        self.app.bind('MasoniteCommandCommand', CommandCommand())
+        self.app.bind('MasoniteControllerCommand', ControllerCommand())
+        self.app.bind('MasoniteInstallCommand', InstallCommand())
+        self.app.bind('MasoniteJobCommand', JobCommand())
+        self.app.bind('MasoniteKeyCommand', KeyCommand())
+        self.app.bind('MasoniteMakeMigrationCommand', MakeMigrationCommand())
+        self.app.bind('MasoniteMigrateCommand', MigrateCommand())
+        self.app.bind('MasoniteMigrateRefreshCommand', MigrateRefreshCommand())
+        self.app.bind('MasoniteMigrateResetCommand', MigrateResetCommand())
+        self.app.bind('MasoniteMigrateRollbackCommand', MigrateRollbackCommand())
+        self.app.bind('MasoniteModelCommand', ModelCommand())
+        self.app.bind('MasoniteProviderCommand', ProviderCommand())
+        self.app.bind('MasoniteServeCommand', ServeCommand())
+        self.app.bind('MasoniteViewCommand', ViewCommand())
 
     def boot(self, Environ, Request, Route):
         self.app.bind('Headers', [])

0d60cbee560dd7520a3b0becc3385b982e89cb5e
Joseph Mancuso <idmann509@gmail.com>
2018-05-18 09:29:02 -0400

    removed sanity text from app collection

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'masonite.contracts',
         'masonite.helpers',
     ],
-    version='1.6.8',
+    version='1.6.9',
     install_requires=[
         'validator.py==1.2.5',
         'cryptography==2.2.2',

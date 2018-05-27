8c8dcc073a955ece22125f93963ce39b89b8dd8d
Joseph Mancuso <idmann509@gmail.com>
2018-05-26 22:39:54 -0400

    added init.py loaders for shorthand classes

---
+++ masonite/managers/__init__.py
@@ -0,0 +1,7 @@
+from .BroadcastManager import BroadcastManager
+from .CacheManager import CacheManager
+from .MailManager import MailManager
+from .Manager import Manager
+from .QueueManager import QueueManager
+from .SessionManager import SessionManager
+from .UploadManager import UploadManager

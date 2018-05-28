cd672225a61ad6a53220bbeac33d90cfe72474cc
Joseph Mancuso <idmann509@gmail.com>
2018-05-28 13:48:47 -0400

    added autoloading instances from a directory

--- masonite/autoload.py
+++ masonite/autoload.py
@@ -6,7 +6,9 @@ from masonite.exceptions import InvalidAutoloadPath
 
 class Autoload:
 
-    def __init__(self, app):
+    classes = {}
+
+    def __init__(self, app=None):
         self.app = app
 
     def load(self, directories):     
@@ -19,3 +21,16 @@ class Autoload:
             for obj in inspect.getmembers(mod):
                 if inspect.isclass(obj[1]):
                     self.app.bind(obj[1].__name__, obj[1])
+    
+    def instances(self, directories, instance):
+        for (module_loader, name, ispkg) in pkgutil.iter_modules(directories):
+            search_path = module_loader.path
+            if search_path.endswith('/'):
+                raise InvalidAutoloadPath('Autoload path cannot have a trailing slash')
+                
+            mod = importlib.import_module(module_loader.path.replace('/', '.') + '.' + name)
+            for obj in inspect.getmembers(mod):
+                if inspect.isclass(obj[1]) and issubclass(obj[1], instance):
+                    self.classes.update({obj[1].__name__: obj[1]})
+
+        return self

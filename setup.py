dc74e30b94ae9176075e9b0b17ac05274eb44b9c
Joseph Mancuso <idmann509@gmail.com>
2018-06-03 23:23:08 -0400

    bumped version into beta 0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         'masonite.contracts',
         'masonite.helpers',
     ],
-    version='2.0.0',
+    version='2.0.0b0',
     install_requires=[
         'validator.py==1.2.5',
         'cryptography==2.2.2',

ff215cd03264d295cb17f9f316932b9ca46447cc
Joseph Mancuso <idmann509@gmail.com>
2018-06-09 20:40:00 -0400

    possibly fixed travis failed test

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -12,7 +12,14 @@ class TestEnvironment:
         assert os.environ.get('LOCAL') == 'TEST'
     
     def test_environment_loads_custom_production_environment(self):
+        env_path = os.path.join(os.getcwd(), '.env')
+        if not os.path.exists(env_path):
+            env_file = open(env_path, 'w')
+            env_file.write('APP_DEBUG=production')
+            env_file.close()
+
         LoadEnvironment()
+
         assert os.environ.get('TEST_PRODUCTION') == 'TEST'
         
     def test_environment_only_loads(self):

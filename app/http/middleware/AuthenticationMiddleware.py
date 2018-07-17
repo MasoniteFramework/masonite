3a34c22cf479ed1a34d3f5b5ae337b5ff1010292
Joseph Mancuso <idmann509@gmail.com>
2018-07-17 11:45:37 -0400

    Update AuthenticationMiddleware.py

--- app/http/middleware/AuthenticationMiddleware.py
+++ app/http/middleware/AuthenticationMiddleware.py
@@ -10,7 +10,7 @@ class AuthenticationMiddleware:
     def before(self):
         ''' Run This Middleware Before The Route Executes '''
         if not self.request.user():
-            self.request.redirectTo('login')
+            self.request.redirect_to('login')
 
     def after(self):
         ''' Run This Middleware After The Route Executes '''

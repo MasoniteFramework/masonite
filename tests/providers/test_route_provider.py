3bf7da3f21d3933ababed44a2fe647e5da2475e9
Joseph Mancuso <idmann509@gmail.com>
2018-08-08 22:08:36 -0400

    adds dashes to routes

--- tests/providers/test_route_provider.py
+++ tests/providers/test_route_provider.py
@@ -96,6 +96,20 @@ class TestRouteProvider:
 
         assert self.app.make('Request').param('endpoint') == 'user.endpoint'
     
+    def test_url_with_dashes_finds_route(self):
+        self.app.make('Route').url = '/test/user-endpoint'
+        self.app.bind('WebRoutes', [get('/test/@endpoint', ControllerTest.show)])
+
+        self.provider.boot(
+            self.app.make('WebRoutes'),
+            self.app.make('Route'),
+            self.app.make('Request'),
+            self.app.make('Environ'),
+            self.app.make('Headers'),
+        )
+
+        assert self.app.make('Request').param('endpoint') == 'user-endpoint'
+    
     def test_route_subdomain_ignores_routes(self):
         self.app.make('Route').url = '/test'
         self.app.make('Environ')['HTTP_HOST'] = 'subb.domain.com'

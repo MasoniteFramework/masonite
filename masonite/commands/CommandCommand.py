f5f892ed3cd0cdb23654c61a67ab251d0aa63e71
Joseph Mancuso <idmann509@gmail.com>
2018-08-16 23:07:59 -0400

    removed comments

--- masonite/commands/CommandCommand.py
+++ masonite/commands/CommandCommand.py
@@ -14,20 +14,3 @@ class CommandCommand(BaseScaffoldCommand):
     scaffold_name = 'Command'
     template = '/masonite/snippets/scaffold/command'
     base_directory = 'app/commands/'
-
-    # def handle(self):
-    #     command = self.argument('name')
-    #     view = View(App())
-    #     command_directory = 'app/commands/{0}.py'.format(command)
-
-    #     if not make_directory(command_directory):
-    #         return self.error('Command Already Exists!')
-
-    #     f = open('app/commands/{0}.py'.format(command), 'w+')
-    #     if view.exists('/masonite/snippets/scaffold/model'):
-    #         f.write(
-    #             view.render('/masonite/snippets/scaffold/command',
-    #                         {'class': command.split('/')[-1]}).rendered_template
-    #         )
-    #         self.info('Command Created Successfully!')
-    #         return f.close()

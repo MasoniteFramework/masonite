f61b7470c6f2ab3f1bcdd7ceb8b72737ff4d1958
Joseph Mancuso <idmann509@gmail.com>
2018-08-16 23:10:20 -0400

    job command now inherits basescaffoldcommand

--- masonite/commands/JobCommand.py
+++ masonite/commands/JobCommand.py
@@ -2,9 +2,7 @@ import os
 
 from cleo import Command
 
-from masonite.app import App
-from masonite.helpers.filesystem import make_directory
-from masonite.view import View
+from masonite.commands import BaseScaffoldCommand
 
 
 class JobCommand(Command):
@@ -15,21 +13,6 @@ class JobCommand(Command):
         {name : Name of the job you want to create}
     """
 
-    def handle(self):
-        job = self.argument('name')
-        view = View(App())
-        job_directory = 'app/jobs/{0}.py'.format(job)
-
-
-        if not make_directory(job_directory):
-            return self.error('Job Already Exists!')
-        
-
-        f = open(job_directory, 'w+')
-        if view.exists('/masonite/snippets/scaffold/job'):
-            f.write(
-                view.render('/masonite/snippets/scaffold/job',
-                            {'class': job.split('/')[-1]}).rendered_template
-            )
-            self.info('Job Created Successfully!')
-            return f.close()
+    scaffold_name = 'Job'
+    template = '/masonite/snippets/scaffold/job'
+    base_directory = 'app/jobs/'

dc3c9576d83e2ac0f94652b55dc21ad71af143f2
Joseph Mancuso <idmann509@gmail.com>
2018-09-09 00:18:40 -0400

    cleaned up random string method

--- masonite/helpers/misc.py
+++ masonite/helpers/misc.py
@@ -3,7 +3,6 @@
 import random, string
 
 def random_string(length=4):
-    rnd_string = "".join(
+    return "".join(
         random.choice(string.ascii_uppercase + string.digits) for _ in range(length)
     )
-    return rnd_string

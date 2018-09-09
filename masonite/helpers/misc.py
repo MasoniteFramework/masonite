dd085c044b687cd18847dda9e56eaedf1b05f0b6
Joseph Mancuso <idmann509@gmail.com>
2018-09-09 00:19:59 -0400

    cleaned up helper method

--- masonite/helpers/misc.py
+++ masonite/helpers/misc.py
@@ -1,8 +1,19 @@
-""" Module for miscellaneous helper methods """ 
+""" Module for miscellaneous helper methods """
 
-import random, string
+import random
+import string
+
 
 def random_string(length=4):
+    """Generates a random string based on the length given
+    
+    Keyword Arguments:
+        length {int} -- The amount of the characters to generate (default: {4})
+    
+    Returns:
+        string
+    """
+    
     return "".join(
         random.choice(string.ascii_uppercase + string.digits) for _ in range(length)
     )

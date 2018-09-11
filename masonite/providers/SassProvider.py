e9e415b82ba846b1a451c76486cba87a2b19b4ef
Joseph Mancuso <idmann509@gmail.com>
2018-09-10 23:02:34 -0400

    fixed docstring comments

--- masonite/providers/SassProvider.py
+++ masonite/providers/SassProvider.py
@@ -9,15 +9,11 @@ class SassProvider(ServiceProvider):
     wsgi = False 
 
     def register(self):
-        """
-        |--------------------------------------------------------------------------
-        | Compile Sass
-        |--------------------------------------------------------------------------
-        |
-        | Compile Sass if the libsass module is installed. Once installed, all
-        | Sass files are compiled when the server is ran. This will only run
-        | once when the server is first started.
-        |
+
+        """Compile Sass
+            Compile Sass if the libsass module is installed. Once installed, all
+            Sass files are compiled when the server is ran. This will only run
+            once when the server is first started.
         """
 
         Storage().compile_sass()

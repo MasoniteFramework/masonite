1a2d71a704786dd65a7642ac04de57763e4a2942
Joseph Mancuso <idmann509@gmail.com>
2018-09-12 23:33:09 -0400

    another another assertion

--- tests/helpers/test_dot_notation.py
+++ tests/helpers/test_dot_notation.py
@@ -5,6 +5,7 @@ def test_dot():
     assert dot('hey.dot', compile_to="{1}[{.}]") == "hey[dot]"
     assert dot('hey.dot.another', compile_to="{1}[{.}]") == "hey[dot][another]"
     assert dot('hey.dot.another.and.another', compile_to="{1}[{.}]") == "hey[dot][another][and][another]"
+    assert dot('hey.dot.another.and.another', compile_to="/{1}[{.}]") == "/hey[dot][another][and][another]"
     assert dot('hey.dot', compile_to="{1}/{.}") == "hey/dot"
     assert dot('hey.dot.another', compile_to="{1}/{.}") == "hey/dot/another"
     assert dot('hey.dot.another', compile_to="{1}/{.}") == "hey/dot/another"

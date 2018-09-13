59184e1076c7132251373152264968794716c9d7
Joseph Mancuso <idmann509@gmail.com>
2018-09-12 23:32:35 -0400

    fixed beginning compile strings

--- masonite/helpers/misc.py
+++ masonite/helpers/misc.py
@@ -3,8 +3,9 @@ def dot(data, compile_to=None):
 
     compiling = ""
     compiling += notation_list[0]
-    dot_split = compile_to.replace('{1}', '').split('{.}')
-
+    beginning_string = compile_to.split('{1}')[0]
+    compiling = beginning_string + compiling
+    dot_split = compile_to.replace(beginning_string + '{1}', '').split('{.}')
     if any(len(x) > 1 for x in dot_split):
         raise ValueError("Cannot have multiple values between {1} and {.}")
 
--- tests/helpers/test_dot_notation.py
+++ tests/helpers/test_dot_notation.py
@@ -8,9 +8,7 @@ def test_dot():
     assert dot('hey.dot', compile_to="{1}/{.}") == "hey/dot"
     assert dot('hey.dot.another', compile_to="{1}/{.}") == "hey/dot/another"
     assert dot('hey.dot.another', compile_to="{1}/{.}") == "hey/dot/another"
-    with pytest.raises(ValueError):
-        assert dot('hey.dot.another',
-                    compile_to="/{1}/{.}") == "hey/dot/another"
+    assert dot('hey.dot.another', compile_to="/{1}/{.}") == "/hey/dot/another"
     with pytest.raises(ValueError):
         assert dot('hey.dot.another',
                     compile_to="{1}//{.}") == "hey/dot/another"

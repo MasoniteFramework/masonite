657630b54e1c54cabd3c85bddc8017e150ed4469
Joseph Mancuso <idmann509@gmail.com>
2018-09-16 22:26:02 -0400

    fixed docstrings

--- routes/api.py
+++ routes/api.py
@@ -1,14 +1,9 @@
 """ API Routes """
 
-"""
-|--------------------------------------------------------------------------
-| Resources
-|--------------------------------------------------------------------------
-|
-| Resources are classes that represent API endpoints. You can create these
-| endpoints with any API Masonite packages. Consider Masonite Entry for
-| creating full RESTful API systems.
-|
+"""Resources
+Resources are classes that represent API endpoints. You can create these
+endpoints with any API Masonite packages. Consider Masonite Entry for
+creating full RESTful API systems.
 """
 
 RESOURCES = []

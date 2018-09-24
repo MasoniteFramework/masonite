e564af625a4fe81a94e9839b4636e678c62fbe6d
Mitchell Dennett <mitch@mylodevelopment.com>
2018-09-24 13:10:04 -0400

    Get Driver from Manager to send mail

--- masonite/auth/MustVerifyEmail.py
+++ masonite/auth/MustVerifyEmail.py
@@ -11,7 +11,8 @@ class MustVerifyEmail:
     """Class To Verify User Email
     """
 
-    def verify_email(self, mail: MailManager, request: Request):
+    def verify_email(self, mail_manager: MailManager, request: Request):
+        mail = mail_manager.helper()
         sign = Sign()
 
         token = sign.sign('{0}::{1}'.format(self.id, time.time()))

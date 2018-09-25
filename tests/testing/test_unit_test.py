02a5476097b54ffd455574c95e838b071fff685a
Joseph Mancuso <idmann509@gmail.com>
2018-09-24 23:23:20 -0400

    fixed unit test

--- tests/testing/test_unit_test.py
+++ tests/testing/test_unit_test.py
@@ -31,9 +31,9 @@ class TestUnitTest(UnitTest):
     def test_unit_test_has_controller(self):
         assert self.route('/testing').has_controller(ControllerTest)
 
-    # def test_user_can_be_loaded(self):
-    #     assert not self.route('/unit/test').user(None).can_view()
-    #     assert self.route('/unit/test').user(MockUser).can_view()
+    def test_user_can_be_loaded(self):
+        assert not self.route('/unit/test').user(None).can_view()
+        assert self.route('/unit/test').user(MockUser).can_view()
 
     def test_view_contains(self):
         assert self.route('/test/route').contains('test')

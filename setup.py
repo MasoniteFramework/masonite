3fdaef01dcb9ed8bc49675edf8c7ed1ea545457a
Joseph Mancuso <idmann509@gmail.com>
2018-11-08 22:21:36 -0500

    bumped pytest version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'python-dotenv>=0.8,<0.9',
         'passlib>=1.7,<1.8',
         'whitenoise>=3.3,<3.4',
-        'pytest>=3,<4',
+        'pytest>=3.6,<4',
         'masonite-entry>=0.0.0,<=0.9.99',
         'masonite-scheduler>=1.0.0,<=1.0.99',
         'pendulum>=1.5,<1.6',

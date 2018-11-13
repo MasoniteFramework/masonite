5d3161b4194d2a76b0f648e7f4daf0b1fb98ddf7
Abram Isola <abram@isola.mn>
2018-11-13 16:58:13 -0600

    Make the reloader allow extra files to be monitored

--- masonite/commands/ServeCommand.py
+++ masonite/commands/ServeCommand.py
@@ -1,5 +1,8 @@
 
-import hupper
+import time
+
+from hupper.logger import DefaultLogger, LogLevel
+from hupper.reloader import Reloader, find_default_monitor_factory
 from cleo import Command
 
 
@@ -18,30 +21,42 @@ class ServeCommand(Command):
         self._check_patch()
 
         if self.option('reload'):
-            logger = hupper.DefaultLogger(1)
+            logger = DefaultLogger(LogLevel.INFO)
 
             # worker args are pickled and then passed to the new process
             worker_args = [
                 self.option("host"), self.option("port"), "wsgi:application",
             ]
 
-            reloader = hupper.Reloader(
+            reloader = Reloader(
                 "masonite.commands._devserver.run",
-                hupper.find_default_monitor_factory(logger),
+                find_default_monitor_factory(logger),
                 logger,
                 worker_args=worker_args,
             )
 
-            # monitor the env file too
-            reloader.watch_files(['.env'])
-
-            # Run the reloader
-            reloader.run()
+            self._run_reloader(reloader, extra_files=[".env"])
 
         else:
             from wsgi import application
             from ._devserver import run
             run(self.option("host"), self.option("port"), application)
+
+    def _run_reloader(self, reloader, extra_files=[]):
+        reloader._capture_signals()
+        reloader._start_monitor()
+        for blob in extra_files:
+            reloader.monitor.add_path(blob)
+        try:
+            while True:
+                if not reloader._run_worker():
+                    reloader._wait_for_changes()
+                time.sleep(reloader.reload_interval)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            reloader._stop_monitor()
+            reloader._restore_signals()
 
     def _check_patch(self):
         patched = False

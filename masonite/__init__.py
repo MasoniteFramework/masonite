0b9673b85dba812c751fcce72f44892bc3095275
Joseph Mancuso <idmann509@gmail.com>
2018-11-15 21:20:33 -0500

    removed request and view from global import

--- masonite/__init__.py
+++ masonite/__init__.py
@@ -5,6 +5,4 @@ from .managers.QueueManager import Queue
 from .managers.SessionManager import Session
 from .managers.UploadManager import Upload
 from masonite.environment import env
-from .request import Request
-from .view import View
 from .provider import ServiceProvider

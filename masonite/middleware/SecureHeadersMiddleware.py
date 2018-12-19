4a73bd7d29915091221f2ddbed5117a38e26694f
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-12-19 12:39:52 +0530

    Create SecureHeadersMiddleware.py

---
+++ masonite/middleware/SecureHeadersMiddleware.py
@@ -0,0 +1,39 @@
+"""SecureHeaders Middleware."""
+
+from masonite.request import Request
+
+from config import middleware
+from config.application import DEBUG
+
+
+class SecureHeadersMiddleware:
+    """SecureHeaders Middleware."""
+
+    def __init__(self, request: Request):
+        """Inject Any Dependencies From The Service Container.
+
+        Arguments:
+            Request {masonite.request.Request} -- The Masonite request object
+        """
+        self.request = request
+
+    def before(self):
+        """Run This Middleware Before The Route Executes.
+        """
+        pass
+
+    def after(self):
+        """Run This Middleware After The Route Executes.
+        """
+        headers = {
+            'Strict-Transport-Security': 'max-age=63072000; includeSubdomains',
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-XSS-Protection': '1; mode=block',
+            'X-Content-Type-Options': 'nosniff',
+            'Referrer-Policy': 'no-referrer, strict-origin-when-cross-origin',
+            'Cache-control': 'no-cache, no-store, must-revalidate',
+            'Pragma': 'no-cache',
+        }
+        headers.update(middleware.SECURE_HEADERS)
+        if not DEBUG:
+            self.request.header(headers)

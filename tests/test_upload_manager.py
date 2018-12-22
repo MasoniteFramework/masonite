6ea2fe92e1b3dfab8c194f44752c0f2474971281
Joseph Mancuso <idmann509@gmail.com>
2018-12-22 17:58:35 -0500

    fixed s3 test

--- tests/test_upload_manager.py
+++ tests/test_upload_manager.py
@@ -146,7 +146,7 @@ if os.environ.get('S3_BUCKET'):
             assert len(self.app.make('Upload').driver('s3').store(ImageMock())) == 29
 
         def test_upload_open_file_for_s3(self):
-            assert self.app.make('Upload').driver('s3').store(open('.travis.yml'))
+            assert self.app.make('Upload').driver('s3').accept('yml').store(open('.travis.yml'))
 
         def test_upload_manage_accept_files(self):
             """

8e864c1471dc8e6aee52ecdad92be837c65b2ed7
Joseph Mancuso <idmann509@gmail.com>
2018-12-22 14:23:46 -0500

    added a cleaner for the request input

--- masonite/helpers/__init__.py
+++ masonite/helpers/__init__.py
@@ -1,6 +1,6 @@
 from .static import static
 from .password import password
 from .validator import validate
-from .misc import random_string, dot
+from .misc import random_string, dot, clean_request_input
 from .Extendable import Extendable
 from .time import cookie_expire_time

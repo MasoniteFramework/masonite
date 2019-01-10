3923890ec3560dc570d304a017abcb7d2840c8d7
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-01-10 05:48:24 +0530

    spring cleaning (#522)
    * first round of spring cleaning
    
    * add location to storage configs
    
    * remove test images
    
    * fix the sessions driver to use memory

--- config/broadcast.py
+++ config/broadcast.py
@@ -1,40 +1,30 @@
-""" Broadcast Settings """
+"""Broadcast Settings."""
 
-import os
+from masonite import env
 
-"""
-|--------------------------------------------------------------------------
-| Broadcast Driver
-|--------------------------------------------------------------------------
-|
-| Realtime support is critical for any modern web application. Broadcast
-| drivers allow you to push data from your server to all your clients
-| to show data updates to your clients in real time without having
-| to constantly refresh the page or send constant ajax requests
-|
-| Supported: 'pusher', 'ably'
-|
+"""Broadcast Driver
+Realtime support is critical for any modern web application. Broadcast
+drivers allow you to push data from your server to all your clients
+to show data updates to your clients in real time without having
+to constantly refresh the page or send constant ajax requests
+
+Supported: 'pusher', 'ably'
 """
 
-DRIVER = os.getenv('BROADCAST_DRIVER', 'pusher')
+DRIVER = env('BROADCAST_DRIVER', 'pusher')
 
-"""
-|--------------------------------------------------------------------------
-| Broadcast Drivers
-|--------------------------------------------------------------------------
-|
-| Below is a dictionary of all your driver configurations. Each key in the
-| dictionary should be the name of a driver.
-|
+"""Broadcast Drivers
+Below is a dictionary of all your driver configurations. Each key in the
+dictionary should be the name of a driver.
 """
 
 DRIVERS = {
     'pusher': {
-        'app_id': os.getenv('PUSHER_APP_ID', '29382xx..'),
-        'client': os.getenv('PUSHER_CLIENT', 'shS8dxx..'),
-        'secret': os.getenv('PUSHER_SECRET', 'HDGdjss..'),
+        'app_id': env('PUSHER_APP_ID', '29382xx..'),
+        'client': env('PUSHER_CLIENT', 'shS8dxx..'),
+        'secret': env('PUSHER_SECRET', 'HDGdjss..'),
     },
     'ably': {
-        'secret': os.getenv('ABLY_SECRET', 'api:key')
+        'secret': env('ABLY_SECRET', 'api:key')
     }
 }
--- config/mail.py
+++ config/mail.py
@@ -1,54 +1,39 @@
-""" Mail Settings """
+"""Mail Settings."""
 
-import os
+from masonite import env
 
-"""
-|--------------------------------------------------------------------------
-| From Address
-|--------------------------------------------------------------------------
-|
-| This value will be used for the default address when sending emails from
-| your application.
-|
+"""From Address
+This value will be used for the default address when sending emails from
+your application.
 """
 
 FROM = {
-    'address': os.getenv('MAIL_FROM_ADDRESS', 'hello@example.com'),
-    'name': os.getenv('MAIL_FROM_NAME', 'Masonite')
+    'address': env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+    'name': env('MAIL_FROM_NAME', 'Masonite')
 }
 
-"""
-|--------------------------------------------------------------------------
-| Mail Driver
-|--------------------------------------------------------------------------
-|
-| The default driver you will like to use for sending emails. You may add
-| additional drivers as you need or pip install additional drivers.
-|
-| Supported: 'smtp', 'mailgun'
-|
+"""Mail Driver
+The default driver you will like to use for sending emails. You may add
+additional drivers as you need or pip install additional drivers.
+
+Supported: 'smtp', 'mailgun'
 """
 
-DRIVER = os.getenv('MAIL_DRIVER', 'smtp')
+DRIVER = env('MAIL_DRIVER', 'smtp')
 
-"""
-|--------------------------------------------------------------------------
-| Mail Drivers
-|--------------------------------------------------------------------------
-|
-| Different drivers you can use for sending email.
-|
+"""Mail Drivers
+Different drivers you can use for sending email.
 """
 
 DRIVERS = {
     'smtp': {
-        'host': os.getenv('MAIL_HOST', 'smtp.mailtrap.io'),
-        'port': os.getenv('MAIL_PORT', '465'),
-        'username': os.getenv('MAIL_USERNAME', 'username'),
-        'password': os.getenv('MAIL_PASSWORD', 'password'),
+        'host': env('MAIL_HOST', 'smtp.mailtrap.io'),
+        'port': env('MAIL_PORT', '465'),
+        'username': env('MAIL_USERNAME', 'username'),
+        'password': env('MAIL_PASSWORD', 'password'),
     },
     'mailgun': {
-        'secret': os.getenv('MAILGUN_SECRET', 'key-XX'),
-        'domain': os.getenv('MAILGUN_DOMAIN', 'sandboxXX.mailgun.org')
+        'secret': env('MAILGUN_SECRET', 'key-XX'),
+        'domain': env('MAILGUN_DOMAIN', 'sandboxXX.mailgun.org')
     }
 }

7b4c046f2556eb1e140f8cf15e3715b011486772
Joseph Mancuso <idmann509@gmail.com>
2019-01-19 12:54:41 -0500

    modified query string parsing

--- masonite/request.py
+++ masonite/request.py
@@ -11,7 +11,7 @@ of this class.
 import re
 from cgi import MiniFieldStorage
 from http import cookies
-from urllib.parse import parse_qs
+from urllib.parse import parse_qsl
 
 import tldextract
 from cryptography.fernet import InvalidToken
@@ -209,11 +209,7 @@ class Request(Extendable):
             variables {string|dict}
         """
         if isinstance(variables, str):
-            variables = parse_qs(variables)
-            t = {}
-            for key, value in variables.items():
-                t[key] = value[0]
-            variables = t
+            variables = dict(parse_qsl(variables))
 
         try:
             for name in variables.keys():
@@ -237,6 +233,8 @@ class Request(Extendable):
             if any(isinstance(storage_obj, MiniFieldStorage) for storage_obj in value):
                 values = [storage_obj.value for storage_obj in value]
 
+                # TODO: This needs to be removed in 2.2. A breaking change but 
+                # this code will result in inconsistent values
                 # If there is only 1 element in the list then return the only value in the list
                 if len(values) == 1:
                     return values[0]

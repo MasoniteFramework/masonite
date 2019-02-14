c271d0d81efa679a94b24fcf23b3e45b65b895b1
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-02-14 23:08:00 +0530

    add -m to craft model

--- masonite/commands/ModelCommand.py
+++ masonite/commands/ModelCommand.py
@@ -1,15 +1,45 @@
 """New Model Command."""
-from masonite.commands import BaseScaffoldCommand
+import subprocess
+
+from masonite.app import App
+from masonite.helpers.filesystem import make_directory
+from masonite.view import View
+
+from cleo import Command
 
 
-class ModelCommand(BaseScaffoldCommand):
+class ModelCommand(Command):
     """
     Creates a model.
 
     model
         {name : Name of the model}
+        {--m|--migration : Create a migration for specified model}
     """
 
     scaffold_name = "Model"
     template = '/masonite/snippets/scaffold/model'
     base_directory = 'app/'
+
+    def handle(self):
+        class_name = self.argument('name')
+        view = View(App())
+        class_directory = '{}{}.py'.format(self.base_directory, class_name)
+
+        if not make_directory(class_directory):
+            return self.error('{0} Already Exists!'.format(self.scaffold_name))
+
+        f = open(class_directory, 'w+')
+        if view.exists(self.template):
+            f.write(
+                view.render(self.template, {
+                            'class': class_name.split('/')[-1]}).rendered_template
+            )
+            self.info('{} Created Successfully!'.format(self.scaffold_name))
+            f.close()
+
+        if self.option('migration'):
+            model_name = class_name.lower() + 's'
+            subprocess.call(['orator', 'make:migration', 'create_{}_table'.format(model_name),
+                             '-p', 'databases/migrations', '--table',
+                             model_name, '--create'])

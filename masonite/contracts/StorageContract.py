518f47843b405716fff70a3b10abf6f8dad8d953
Joseph Mancuso <idmann509@gmail.com>
2019-03-02 08:01:01 -0500

    updated storage contract

--- masonite/contracts/StorageContract.py
+++ masonite/contracts/StorageContract.py
@@ -5,23 +5,50 @@ from abc import abstractmethod
 class StorageContract(Contract):
 
     @abstractmethod
-    def put(self):
+    def put(self, location, contents):
+        """Puts a file into the correct directory
+
+        Arguments:
+            location {string} -- The location of the file
+            contents {string|object|file-like object} -- The file object to add.
+        """
         pass
 
     @abstractmethod
-    def get(self):
+    def get(self, location):
+        """Get the file contents
+
+        Arguments:
+            location {string} -- The location of the file
+        """
         pass
 
     @abstractmethod
-    def append(self):
+    def append(self, location, contents):
+        """Get the file contents
+
+        Arguments:
+            location {string} -- The location of the file.
+            contents {string|object|file-like object} -- The file object to add.
+        """
         pass
 
     @abstractmethod
-    def delete(self):
+    def delete(self, location):
+        """Deletes the file.
+
+        Arguments:
+            location {string} -- The location of the file.
+        """
         pass
 
     @abstractmethod
-    def exists(self):
+    def exists(self, location):
+        """Checks if a file exists.
+
+        Arguments:
+            location {string} -- The location of the file.
+        """
         pass
 
     @abstractmethod
@@ -29,33 +56,86 @@ class StorageContract(Contract):
         pass
 
     @abstractmethod
-    def url(self):
+    def url(self, location):
+        """Gets the full URL of the file to be served.
+
+        Arguments:
+            location {string} -- The location of the file.
+        """
         pass
 
     @abstractmethod
-    def size(self):
+    def size(self, location):
+        """Gets the size of the file.
+
+        Arguments:
+            location {string} -- The location of the file.
+        """
         pass
 
     @abstractmethod
-    def extension(self):
+    def extension(self, location):
+        """Gets the extension of the file.
+
+        Arguments:
+            location {string} -- The location of the file.
+        """
         pass
 
     @abstractmethod
-    def upload(self):
+    def upload(self, *args, **kwargs):
+        """Passes all arguments to the upload version of this storage driver.
+
+        Arguments:
+            location {string} -- The location of the file.
+        """
         pass
 
     @abstractmethod
-    def all(self):
+    def all(self, location):
+        """Gets all files in a specific directory
+
+        Arguments:
+            location {string} -- The location of the directory.
+        """
         pass
 
     @abstractmethod
-    def make_directory(self):
+    def make_directory(self, directory):
+        """Make an empty directory
+
+        Arguments:
+            directory {string} -- The location of the directory.
+        """
         pass
 
     @abstractmethod
-    def delete_directory(self):
+    def delete_directory(self, directory, force=False):
+        """Delete a directory.
+
+        Arguments:
+            directory {string} -- The location of the directory
+
+        Keyword Arguments:
+            force {bool} -- Whether or not a directory with contents should be deleted. (default: {False})
+        """
         pass
 
     @abstractmethod
-    def move(self):
+    def move(self, old, new):
+        """Move a file from 1 location to another.
+
+        Arguments:
+            old {string} -- The file of the file object to be moved.
+            new {string} -- The location where the file object should be moved to.
+        """
+        pass
+
+    @abstractmethod
+    def name(self, location):
+        """Gets the name of the file with the extension
+
+        Arguments:
+            location {string} -- The location of the file
+        """
         pass

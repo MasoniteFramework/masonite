807c55a89409a3134a6cd26c69d0f7a1162cb79d
Joseph Mancuso <idmann509@gmail.com>
2019-03-31 08:05:43 -0400

    flake8

--- masonite/commands/ServeCommand.py
+++ masonite/commands/ServeCommand.py
@@ -23,7 +23,7 @@ class ServeCommand(Command):
     def handle(self):
         if has_unmigrated_migrations():
             self.comment("\nYou have unmigrated migrations. Run 'craft migrate' to migrate them\n")
-        
+
         if not self.option('dont-reload'):
             logger = DefaultLogger(LogLevel.INFO)
 

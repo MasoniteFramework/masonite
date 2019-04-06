4cebbc9ab519f2e2323b17e0fc2d0515cca0294b
Joseph Mancuso <idmann509@gmail.com>
2019-04-06 15:22:16 -0400

    fixed how broadcasting gets it's configurtions

--- tests/broadcasts/test_sockets.py
+++ tests/broadcasts/test_sockets.py
@@ -1,34 +1,30 @@
 import os
 
-from config import broadcast
 from masonite.drivers import BroadcastAblyDriver, BroadcastPusherDriver
 from masonite.managers import BroadcastManager
 from masonite.testsuite.TestSuite import TestSuite
 import unittest
 
+if os.getenv('ABLY_SECRET'):
 
+    class TestSockets(unittest.TestCase):
+            
+        def setUp(self):
+            self.app = TestSuite().create_container().container
+            self.app.bind('BroadcastPusherDriver', BroadcastPusherDriver)
+            self.app.bind('BroadcastManager', BroadcastManager().load_container(self.app))
 
-# if os.getenv('ABLY_SECRET'):
+        def test_broadcast_loads_into_container(self):
+            self.app.bind('Broadcast', self.app.make('BroadcastManager').driver('pusher'))
 
-class TestSockets(unittest.TestCase):
-        
-    def setUp(self):
-        self.app = TestSuite().create_container().container
-        self.app.bind('BroadcastConfig', broadcast)
-        self.app.bind('BroadcastPusherDriver', BroadcastPusherDriver)
-        self.app.bind('BroadcastManager', BroadcastManager().load_container(self.app))
+            self.assertIsNotNone(self.app.make('BroadcastManager'))
+            self.assertEqual(self.app.make('Broadcast').channel('random', 'from driver'), {'message': 'from driver'})
+            self.assertEqual(self.app.make('Broadcast').channel('random', {'message': 'dictionary'}), {'message': 'dictionary'})
+            self.assertEqual(self.app.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}), {'message': 'dictionary'})
+            self.assertEqual(self.app.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}, 'test-event'), {'message': 'dictionary'})
+            self.assertTrue(self.app.make('Broadcast').ssl(True).ssl_message)
 
-    def test_broadcast_loads_into_container(self):
-        self.app.bind('Broadcast', self.app.make('BroadcastManager').driver('pusher'))
-
-        self.assertIsNotNone(self.app.make('BroadcastManager'))
-        self.assertEqual(self.app.make('Broadcast').channel('random', 'from driver'), {'message': 'from driver'})
-        self.assertEqual(self.app.make('Broadcast').channel('random', {'message': 'dictionary'}), {'message': 'dictionary'})
-        self.assertEqual(self.app.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}), {'message': 'dictionary'})
-        self.assertEqual(self.app.make('Broadcast').channel(['channel1', 'channel2'], {'message': 'dictionary'}, 'test-event'), {'message': 'dictionary'})
-        self.assertTrue(self.app.make('Broadcast').ssl(True).ssl_message)
-
-    def test_broadcast_loads_into_container_with_ably(self):
+        def test_broadcast_loads_into_container_with_ably(self):
             self.app.bind('Broadcast', self.app.make('BroadcastManager').driver('ably'))
 
             self.assertIsNotNone(self.app.make('BroadcastManager'))

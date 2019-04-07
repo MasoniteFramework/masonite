4753f6b828b0251197913614ee980e82aa206900
Joseph Mancuso <idmann509@gmail.com>
2019-04-06 23:32:14 -0400

    added last test

--- tests/core/test_validator.py
+++ tests/core/test_validator.py
@@ -1,24 +1,27 @@
-from masonite.request import Request
-from masonite.validator import Validator
-from validator import Pattern, Required, Length
-from masonite.testsuite.TestSuite import generate_wsgi
+import unittest
+
+from validator import Length, Pattern, Required
+
 from masonite.helpers import validate
+from masonite.request import Request
+from masonite.testsuite.TestSuite import generate_wsgi
+from masonite.validator import Validator
 
 
-class TestValidator:
+class TestValidator(unittest.TestCase):
 
-    def setup_method(self):
+    def setUp(self):
         self.request = Request(generate_wsgi())
         self.request.request_variables = {'id': '1'}
 
     def test_validator_sets_request(self):
         validator = Validator(self.request)
-        assert validator.request is self.request
+        self.assertIsInstance(validator.request, self.request)
 
     def test_email_validator_sets_dictionary(self):
         email_validator = Validator(self.request)
         email_validator.validate({'id': 1})
-        assert email_validator.validation_dictionary == {'id': 1}
+        self.assertEqual(email_validator.validation_dictionary, {'id': 1})
 
     def test_validator_check_does_not_match(self):
         email_validator = Validator(self.request)
@@ -34,13 +37,13 @@ class TestValidator:
         email_validator = Validator(self.request)
         email_validator.validate({'username': [Required]})
         assert email_validator.check() is False
-        assert email_validator.errors()['username'] == 'must be present'
+        self.assertEqual(email_validator.errors()['username'], 'must be present')
 
     def test_validator_error_without_request(self):
         email_validator = Validator()
         email_validator.validate({'username': [Required]})
         assert email_validator.check({'id': 5}) is False
-        assert email_validator.errors()['username'] == 'must be present'
+        self.assertEqual(email_validator.errors()['username'], 'must be present')
 
     def test_validator_check_matches_without_request(self):
         email_validator = Validator()
@@ -60,9 +63,7 @@ class TestValidator:
             {'id': 'change your id', 'username': 'dont forget your username'})
 
         assert email_validator.check({'tomato': 'true'}) is False
-        assert email_validator.errors()['id'] == 'change your id'
-        assert email_validator.errors(
-        )['username'] == 'dont forget your username'
+        self.assertEqual(email_validator.errors()['id'], 'change your id')
 
     def test_custom_error_messages_missing_one(self):
         email_validator = Validator()
@@ -72,7 +73,7 @@ class TestValidator:
             {'username': 'dont forget your username'})
 
         assert email_validator.check({'username': '5'}) is False
-        assert email_validator.errors()['id'] == 'must be present'
+        self.assertEqual(email_validator.errors()['id'], 'must be present')
 
     def test_validator_length(self):
         email_validator = Validator()
@@ -89,28 +90,28 @@ class TestValidator:
         email_validator = CastValidator(self.request)
         email_validator.test()
         assert email_validator.check()
-        assert email_validator.get('id') == ['1', '2']
-        assert self.request.all() == {'id': '1,2'}
+        self.assertEqual(email_validator.get('id'), ['1', '2'])
+        self.assertEqual(self.request.all(), {'id': '1,2'})
 
     def test_validator_can_get_validation_error(self):
         email_validator = Validator()
         email_validator.validate({'id': [Required]})
         assert email_validator.check({'name': '1,2'}) is False
-        assert email_validator.error('id') == 'must be present'
-        assert email_validator.error('name') == None
+        self.assertEqual(email_validator.error('id'), 'must be present')
+        self.assertEqual(email_validator.error('name'), None)
 
 
-class TestValidatorHelper:
+class TestValidatorHelper(unittest.TestCase):
 
     def test_helper_validates_data(self):
-        assert validate({'id': [Required]}, {'name': '1,2'}) == {
-            'id': 'must be present'}
+        self.assertEqual(validate({'id': [Required]}, {'name': '1,2'}), {
+            'id': 'must be present'})
 
-        assert validate({'id': [Required]}, {'id': '1,2'}) == True
+        self.assertEqual(validate({'id': [Required]}, {'id': '1,2'}), True)
 
     def test_helper_validates_with_custom_messages(self):
-        assert validate({'id': [Required]}, {'name': '1,2'}, {'id': 'is required'}) == {
-            'id': 'is required'}
+        self.assertEqual(validate({'id': [Required]}, {'name': '1,2'}, {'id': 'is required'}), {
+            'id': 'is required'})
 
 
 class CastValidator(Validator):

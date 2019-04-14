dea86c8d3097da418ff01e453bb1fcd433ce6502
Joseph Mancuso <idmann509@gmail.com>
2019-04-14 09:41:30 -0400

    flake8

--- masonite/drivers/upload/BaseUploadDriver.py
+++ masonite/drivers/upload/BaseUploadDriver.py
@@ -23,11 +23,11 @@ class BaseUploadDriver(BaseDriver):
         if '*' in extensions and len(extensions) > 1:
             raise ValueError("When uses '*' isn't allowed accept other file type.")
 
-        if ( extensions == ['*'] ):
+        if (extensions == ['*']):
             self.accept_file_types = None
         else:
             self.accept_file_types = args
-        
+
         return self
 
     def validate_extension(self, filename):

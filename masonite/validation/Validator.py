ae2a2c9a4d70d87c029c0b46fb4108d46d1c4938
Joseph Mancuso <idmann509@gmail.com>
2019-05-03 22:58:39 -0400

    rewrote validation

--- masonite/validation/Validator.py
+++ masonite/validation/Validator.py
@@ -1,49 +1,91 @@
-class Required:
-    error = "The element in required"
+class BaseValidation:
 
-    def handle(self, key, dictionary, negation=False):
-        if negation:
-            return True
+    def __init__(self, validations):
+        self.errors = []
+        # validations = ['user', 'email']
+        self.validations = validations
 
-        return key in dictionary
+    def error(self, message):
+        self.errors.append(message)
+
+class required(BaseValidation):
+
+    def handle(self, dictionary):
+        boolean = True
+        for key in self.validations:
+            if not key in dictionary:
+                boolean = False
+                self.error('{} is required'.format(key))
+
+        return boolean
 
 
-class Numeric:
-    error = "The element should be a numeric element"
+class numeric(BaseValidation):
 
-    def handle(self, key, dictionary, negation=False):
-        return key in dictionary and str(dictionary[key]).isdigit()
+    def handle(self, dictionary, negation=False):
+        boolean = True
 
-class JSONValidator:
-    error = "The element needs to be a JSON object"
+        for key in self.validations:
+            if key in dictionary and not str(dictionary[key]).isdigit():
+                boolean = False
+                self.error('{} must be a numeric'.format(key))
 
-    def handle(self, key, dictionary, negation=False):
+        return boolean
+
+    
+class string(BaseValidation):
+
+    def handle(self, dictionary):
+        boolean = True
+
+        for key in self.validations:
+            if key in dictionary and not isinstance(dictionary[key], str):
+                boolean = False
+                self.error('{} must be a string'.format(key))
+
+        return boolean
+
+class length(BaseValidation):
+
+    def __init__(self, validations, min=1, max=255):
+        super().__init__(validations)
+        self.min = min
+        self.max = max
+
+    def handle(self, dictionary, negation=False):
+        boolean = True
+
+        for key in self.validations:
+            if key in dictionary and (len(dictionary[key]) < self.min or len(dictionary[key]) > self.max):
+                boolean = False
+                self.error('{} length must be between {} and {}'.format(key, self.min, self.max))
+
+        return boolean
+
+class json(BaseValidation):
+
+    def handle(self, dictionary):
         import json
+        boolean = True
         try:
-            return key in dictionary and json.loads(dictionary[key])
-        except TypeError:
-            return True if negation else False
-
+            for key in self.validations:
+                if key in dictionary and not json.loads(dictionary[key]):
+                    boolean = False
+                    self.error('{} must be json'.format(key))
+            
+            return boolean
+        except (TypeError, json.decoder.JSONDecodeError):
+            self.error('{} must be json'.format(key))
+            return False
 
 
 class Validator:
-    validations = {
-        'required': Required,
-        'numeric': Numeric,
-        'json': JSONValidator,
-    }
 
-    def __init__(self, dictionary, rules):
-        self.errors = {}
-        self.validate(dictionary, rules)
+    def __init__(self, dictionary, *rules):
+        self.errors = []
+        self.validate(dictionary, *rules)
 
-    def validate(self, dictionary, rules):
-        self.rules = rules
-
-        for key, r in rules.items():
-            split_rules = r.split('|')
-            for rule in split_rules:
-                x = self.validations[rule.replace('not_', '')]()
-
-                if not x.handle(key, dictionary, negation=rule.startswith('not_')):
-                    self.errors.update({key: x.error})
+    def validate(self, dictionary, *rules):
+        for rule in rules:
+            if not rule.handle(dictionary):
+                self.errors += rule.errors
--- tests/validation/test_required.py
+++ tests/validation/test_required.py
@@ -1,62 +1,82 @@
 
 import unittest
-from masonite.validation.Validator import Validator
+from masonite.validation.Validator import Validator, required, numeric, json as vjson, length, string
+import json
 
-class TestRequired(unittest.TestCase):
+class TestValidation(unittest.TestCase):
 
     def setUp(self):
         pass
     
-    def test_required_calls_required(self):
+    def test_required(self):
         validate = Validator({
             'test': 1
-        }, {
-            'user': 'required'
-        })
+        }, required(['user', 'email']))
 
-        self.assertEqual(validate.errors['user'], 'The element in required')
+        self.assertEqual(validate.errors, ['user is required', 'email is required'])
 
         validate = Validator({
-            'test': 1,
-            'user': 'hey'
-        }, {
-            'user': 'not_required'
-        })
+            'test': 1
+        }, required(['test']))
 
-        self.assertNotIn('user', validate.errors)
+        self.assertEqual(len(validate.errors), 0)
 
-    def test_numeric_calls_numeric(self):
+    def test_numeric(self):
         validate = Validator({
-            'user': 1
-        }, {
-            'user': 'numeric'
-        })
+            'test': 1
+        }, numeric(['test']))
 
-        self.assertNotIn('user', validate.errors)
-
-    def test_several_rules(self):
-        validate = Validator({
-            'user': 1
-        }, {
-            'user': 'required|numeric'
-        })
-
-        self.assertNotIn('user', validate.errors)
-
-    def test_json_rule(self):
-        import json
-        validate = Validator({
-            'user': json.dumps({'user': 1})
-        }, {
-            'user': 'required|json'
-        })
-
-        self.assertNotIn('user', validate.errors)
+        self.assertEqual(len(validate.errors), 0)
 
         validate = Validator({
-            'user': 1
-        }, {
-            'user': 'required|json'
-        })
+            'test': 'hey'
+        }, numeric(['test']))
 
-        self.assertEqual(validate.errors['user'], 'The element needs to be a JSON object')
+        self.assertEqual(validate.errors, ['test must be a numeric'])
+
+    def test_several_tests(self):
+        validate = Validator({
+            'test': 'hey'
+        }, required(['notin']), numeric(['notin']))
+
+        self.assertEqual(validate.errors, ['notin is required'])
+
+    def test_json(self):
+        validate = Validator({
+            'json': 'hey'
+        }, vjson(['json']))
+
+        self.assertEqual(validate.errors, ['json must be json'])
+
+        validate = Validator({
+            'json': json.dumps({'test': 'key'})
+        }, vjson(['json']))
+
+        self.assertEqual(len(validate.errors), 0)
+
+    def test_length(self):
+        validate = Validator({
+            'json': 'hey'
+        }, length(['json'], min=1, max=10))
+
+        self.assertEqual(len(validate.errors), 0)
+
+        validate = Validator({
+            'json': 'this is a really long string'
+        }, length(['json'], min=1, max=10))
+
+        self.assertEqual(validate.errors, ['json length must be between 1 and 10'])
+
+    def test_string(self):
+        validate = Validator({
+            'text': 'hey'
+        }, string(['text']))
+
+        self.assertEqual(len(validate.errors), 0)
+
+        validate = Validator({
+            'text': 1
+        }, string(['text']))
+
+        self.assertEqual(validate.errors, ['text must be a string'])
+

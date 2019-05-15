cf0d54d42c43fb84aead4d6d99b34a73b06b0d54
Joseph Mancuso <idmann509@gmail.com>
2019-05-14 21:57:26 -0400

    removed validator.py from the application

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     version=VERSION,
     install_requires=[
         'masonite-events>=1.0,<2',
-        'validator.py>=1.2,<1.3',
         'cryptography>=2.3,<2.4',
         'bcrypt>=3.1,<3.2',
         'requests>=2.0,<2.99',

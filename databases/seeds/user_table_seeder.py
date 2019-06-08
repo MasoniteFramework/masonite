cc974dba3b5aa410434f6be07bb37155488c6721
Joseph Mancuso <idmann509@gmail.com>
2019-06-08 13:53:47 -0400

    changed tests to use factories

--- databases/seeds/user_table_seeder.py
+++ databases/seeds/user_table_seeder.py
@@ -7,10 +7,10 @@ You can run this seeder in order to generate users.
     - You can run the seeder by running: craft seed:run.
 """
 
-from orator.orm import Factory
 from orator.seeds import Seeder
 
 from app.User import User
+from config.factories import factory
 
 
 class UserTableSeeder(Seeder):
@@ -19,13 +19,4 @@ class UserTableSeeder(Seeder):
         """
         Run the database seeds.
         """
-        self.factory.register(User, self.users_factory)
-
-        self.factory(User, 50).create()
-
-    def users_factory(self, faker):
-        return {
-            'name': faker.name(),
-            'email': faker.email(),
-            'password': '$2b$12$WMgb5Re1NqUr.uSRfQmPQeeGWudk/8/aNbVMpD1dR.Et83vfL8WAu',  # == 'secret'
-        }
+        factory(User, 50).create()
--- tests/database/test_user.py
+++ tests/database/test_user.py
@@ -3,6 +3,7 @@
 from masonite.testing import TestCase
 
 from app.User import User
+from config.factories import factory
 
 
 class TestUser(TestCase):
@@ -17,22 +18,7 @@ class TestUser(TestCase):
         """This runs when the test class first starts up.
         This does not run before every test case.
         """
-        self.make(User, self.users_factory, 1)
-
-    def users_factory(self, faker):
-        """Example factory
-
-        Arguments:
-            faker {faker.Faker} -- An instance of Faker
-
-        Returns:
-            dict
-        """
-        return {
-            'name': faker.name(),
-            'email': faker.email(),
-            'password': '$2b$12$WMgb5Re1NqUr.uSRfQmPQeeGWudk/8/aNbVMpD1dR.Et83vfL8WAu',  # == 'secret'
-        }
+        factory(User, 1).create()
 
     def test_created_user(self):
         self.assertTrue(User.find(1))

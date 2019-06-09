2ac4f8a458f774e30efa41dd55389c1fcedc149a
Joseph Mancuso <idmann509@gmail.com>
2019-06-09 08:09:45 -0400

    linted and bumped version

--- masonite/__version__.py
+++ masonite/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'masonite'
 __description__ = 'The core for the Masonite framework'
 __url__ = 'https://github.com/MasoniteFramework/masonite'
-__version__ = '2.2.0b3.post2'
+__version__ = '2.2.0b3.post3'
 __author__ = 'Joseph Mancuso'
 __author_email__ = 'idmann509@gmail.com'
 __licence__ = 'MIT'

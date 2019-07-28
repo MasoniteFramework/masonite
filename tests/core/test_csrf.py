691d64ba3b3e113125094d4bf95ff4e540b91e16
Joseph Mancuso <idmann509@gmail.com>
2019-07-27 22:40:57 -0400

    fixed issue with dashes in csrf exempt

--- tests/core/test_csrf.py
+++ tests/core/test_csrf.py
@@ -3,29 +3,36 @@ import unittest
 from masonite.auth.Csrf import Csrf
 from masonite.middleware import CsrfMiddleware
 from masonite.testsuite.TestSuite import TestSuite
+from masonite.testing import TestCase
+from masonite.routes import Get, Post
 
 
-class TestCsrf(unittest.TestCase):
+class TestCsrf(TestCase):
 
     def setUp(self):
-        self.app = TestSuite().create_container().container
-        self.app.bind('Csrf', Csrf(self.app.make('Request')))
-        self.csrf = self.app.make('Csrf')
-        self.request = self.app.make('Request')
-        middleware = self.app.resolve(CsrfMiddleware)
-
-        middleware.before()
+        super().setUp()
+        self.routes(only=[Get('/', 'ControllerTest@show').middleware('csrf'), Post('/test-route', 'ControllerTest@show').middleware('csrf')])
+        csrf = CsrfMiddleware
+        csrf.exempt = ['/*']
+        self.withRouteMiddleware({'csrf': csrf})
 
     def test_middleware_sets_csrf_cookie(self):
-        self.assertTrue(self.request.get_cookie('csrf_token', decrypt=False))
-
+        self.assertTrue(
+            self.get('/').container.make('Request').get_cookie('csrf_token', decrypt=False)
+        )
+ 
     def test_middleware_shares_view(self):
-        self.assertIn('csrf_field', self.app.make('ViewClass')._shared)
-        self.assertIn('input', self.app.make('ViewClass')._shared['csrf_field'])
+        self.assertIn('csrf_field', self.get('/').container.make('ViewClass')._shared)
 
     def test_middleware_does_not_need_safe_filter(self):
-        self.assertNotIn('&lt;', self.app.make('ViewClass').render('csrf_field').rendered_template)
+        self.assertNotIn('&lt;', self.container.make('ViewClass').render('csrf_field').rendered_template)
 
     def test_verify_token(self):
-        token = self.request.get_cookie('csrf_token', decrypt=False)
-        self.assertTrue(self.csrf.verify_csrf_token(token))
+        token = self.get('/').container.make('Request').get_cookie('csrf_token', decrypt=False)
+        self.assertTrue(self.container.make('Csrf').verify_csrf_token(token))
+
+    def test_csrf_with_dashes(self):
+        (self.withCsrf()
+            .withoutHttpMiddleware()
+            .post('/test-route'))
+        # self.assertTrue(self.container.make('Csrf').verify_csrf_token(token))

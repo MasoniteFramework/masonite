aba8ff061b04c803aac3bf7692924d840946fc1c
Joseph Mancuso <idmann509@gmail.com>
2019-08-04 17:40:18 -0400

    fixed issue with circlular dependency

--- masonite/auth/MustVerifyEmail.py
+++ masonite/auth/MustVerifyEmail.py
@@ -3,14 +3,12 @@
 import time
 
 from masonite.auth.Sign import Sign
-from masonite.managers import MailManager
-from masonite.request import Request
 
 
 class MustVerifyEmail:
     """Class To Verify User Email."""
 
-    def verify_email(self, mail_manager: MailManager, request: Request):
+    def verify_email(self, mail_manager, request):
         """Sends email for user verification
 
         Arguments:

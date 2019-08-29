20d16cb2a13c71ab47213f9fb331200be2cbeb36
Joseph Mancuso <idmann509@gmail.com>
2019-08-28 21:29:27 -0400

    linted

--- masonite/routes.py
+++ masonite/routes.py
@@ -472,7 +472,7 @@ class Options(BaseHttpRoute):
         self.list_middleware = []
         if route is not None and output is not None:
             self.route(route, output)
-        
+
         print('The Masonite development server is not capable of handling OPTIONS preflight requests.')
         print('You should use a more powerful server if using the Option')
 

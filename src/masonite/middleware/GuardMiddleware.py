8582535d7d23c01d5f5c13474080dfdb34a405a2
Joseph Mancuso <idmann509@gmail.com>
2019-11-02 14:40:06 -0400

    added comments

--- src/masonite/middleware/GuardMiddleware.py
+++ src/masonite/middleware/GuardMiddleware.py
@@ -10,7 +10,17 @@ class GuardMiddleware:
         self.auth = auth
 
     def before(self, guard):
+        """Sets specified guard for the request.
+        
+        Arguments:
+            guard {string} -- The key of the guard to set.
+        """
         self.auth.set(guard)
 
-    def after(self, guard):
+    def after(self, _):
+        """Sets the default guard back after the request.
+        
+        Arguments:
+            _ {ignored} -- ignored
+        """
         self.auth.set(config('auth.auth.defaults.guard'))

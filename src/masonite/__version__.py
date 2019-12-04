b174ecaffe02a7c8b121e325bf7c5882b1fd899a
Joseph Mancuso <idmann509@gmail.com>
2019-12-03 23:20:02 -0500

    bumped version

--- src/masonite/__version__.py
+++ src/masonite/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'masonite'
 __description__ = 'The core for the Masonite framework'
 __url__ = 'https://github.com/MasoniteFramework/masonite'
-__version__ = '2.3.0b2'
+__version__ = '2.3.0b2.post1'
 __author__ = 'Joseph Mancuso'
 __author_email__ = 'joe@masoniteproject.com'
 __licence__ = 'MIT'

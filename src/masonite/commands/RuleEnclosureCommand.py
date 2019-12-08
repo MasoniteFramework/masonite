acc1ca644be66c12de25438b0544c3d32cfd33c4
Samuel Girardin <samuelgirardin@gmail.com>
2019-12-08 17:19:57 +0100

    Add ValidationProvider in default config

---
+++ src/masonite/commands/RuleEnclosureCommand.py
@@ -0,0 +1,17 @@
+"""New Model Command."""
+
+from ..commands import BaseScaffoldCommand
+
+
+class RuleEnclosureCommand(BaseScaffoldCommand):
+    """
+    Creates a new rule enclosure.
+
+    rule:enclosure
+        {name : Name of the rule enclosure}
+    """
+
+    scaffold_name = 'Rule'
+    postfix = ""
+    template = '/masonite/validation/snippets/scaffold/rule_enclosure'
+    base_directory = 'app/rules/'

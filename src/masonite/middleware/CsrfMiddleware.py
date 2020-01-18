c3a36245cf09b3c2b766587745e64d0a69f72343
Joseph Mancuso <idmann509@gmail.com>
2020-01-18 07:57:29 -0500

    fixed looking up of xsrf token

--- src/masonite/middleware/CsrfMiddleware.py
+++ src/masonite/middleware/CsrfMiddleware.py
@@ -72,7 +72,8 @@ class CsrfMiddleware:
         """
 
         if self.request.is_post() and not self.in_exempt():
-            token = self.request.header('HTTP_X_CSRF_TOKEN') or self.request.input('__token')
+            token = self.request.header('HTTP_X_CSRF_TOKEN') or self.request.header(
+                'HTTP_X_XSRF_TOKEN') or self.request.input('__token')
             if not self.csrf.verify_csrf_token(token):
                 raise InvalidCSRFToken("Invalid CSRF token.")
             return token

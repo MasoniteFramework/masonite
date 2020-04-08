aec112fc5887f1e1ce20c124f903656e0c54442b
Marlysson <marlysson5@gmail.com>
2020-04-08 17:16:59 -0300

    Separate the method that init the whole environment to test cases to be called inside the setUp method on TestCase baseclass and change the scaffold test.html to command the setUp method to be uncommented case necessary. closes #279.

--- src/masonite/testing/TestCase.py
+++ src/masonite/testing/TestCase.py
@@ -24,6 +24,9 @@ class TestCase(unittest.TestCase):
     _transaction = False
 
     def setUp(self):
+        self.initConfigs()
+
+    def initConfigs(self):
         from wsgi import container
         self.container = container
         self._with_subdomains = False

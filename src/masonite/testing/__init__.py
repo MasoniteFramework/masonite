cc45bd1f26411c245969aa51d9e39a62df9176f0
Aron Podrigal <aronp@guaranteedplus.com>
2020-06-18 13:52:24 -0400

    Fixed raw_input to reset for each request

--- src/masonite/testing/__init__.py
+++ src/masonite/testing/__init__.py
@@ -1,4 +1,4 @@
 from .TestCase import TestCase
 from .MockRoute import MockRoute
-from .generate_wsgi import generate_wsgi
+from .generate_wsgi import generate_wsgi, MockWsgiInput
 from .create_container import create_container

5be299a96a823041ed3e1905e66df3720e29342e
Samuel Girardin <samuelgirardin@pm.me>
2020-10-24 11:14:05 +0200

    refactor using github api and add gitlab api

--- src/masonite/commands/NewCommand.py
+++ src/masonite/commands/NewCommand.py
@@ -20,6 +20,10 @@ class NewCommand(Command):
 
     providers = ["github", "gitlab"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.api_base_url = None
+
     def handle(self):
         name = self.argument("name")
         if not name:
@@ -50,75 +54,61 @@ class NewCommand(Command):
                 return self.comment(
                     'There is a folder that starts with "masonite-" and therefore craft cannot create a new project'
                 )
-        try:
-            if repo:
-                if provider not in self.providers:
-                    return self.comment("provider must be in {}".format(",".join(self.providers)))
-                if provider == "github":
-                    # master branch is cloned by default
-                    zipball = "https://{}.com/{}/archive/master.zip".format(provider, repo)
-                else:
-                    raise NotImplementedError("Crafting project from this repo provider is not implemented yet.")
+        # try:
+        if repo and provider not in self.providers:
+            return self.comment("provider must be in {}".format(",".join(self.providers)))
 
-            elif branch != "False":
-                get_branch = requests.get(
-                    "https://api.github.com/repos/MasoniteFramework/cookie-cutter/branches/{0}".format(
-                        branch
+        self.set_api_provider_url_for_repo(provider, repo)
+
+        if branch != "False":
+            branch_data = self.get_branch_provider_data(provider, branch)
+            if "name" not in branch_data.json():
+                return self.comment("Branch {0} does not exist.".format(branch))
+
+            zipball = self.get_branch_archive_url(provider, repo, branch)
+        elif version != "False":
+            releases_data = self.get_releases_provider_data(provider)
+            zipball = False
+
+            for release in releases_data:
+                if "tag_name" in release and release["tag_name"].startswith(
+                    "v{0}".format(version)
+                ):
+                    self.info(
+                        "Installing version {0}".format(release["tag_name"])
                     )
+                    self.line("")
+                    zipball = self.get_release_archive_url_from_release_data(provider, release)
+                    break
+
+            if zipball is False:
+                return self.info(
+                    "Version {0} could not be found".format(version)
                 )
+        else:
+            tags_data = self.get_releases_provider_data(provider)
 
-                if "name" not in get_branch.json():
-                    return self.comment("Branch {0} does not exist.".format(branch))
+            # try to find all releases which are not prereleases
+            tags = []
+            for release in tags_data:
+                if release["prerelease"] is False:
+                    tag_key = "tag_name" if provider == "github" else "name"
+                    tags.append(release[tag_key].replace("v", ""))
 
-                zipball = "https://github.com/MasoniteFramework/cookie-cutter/archive/{0}.zip".format(
-                    branch
-                )
-            elif version != "False":
-                get_zip_url = requests.get(
-                    "https://api.github.com/repos/MasoniteFramework/cookie-cutter/releases"
-                )
-                zipball = False
-
-                for release in get_zip_url.json():
-                    if "tag_name" in release and release["tag_name"].startswith(
-                        "v{0}".format(version)
-                    ):
-                        self.info(
-                            "Installing version {0}".format(release["tag_name"])
-                        )
-                        self.line("")
-                        zipball = release["zipball_url"]
-                        break
-
-                if zipball is False:
-                    return self.info(
-                        "Version {0} could not be found".format(version)
-                    )
-            else:
-                get_zip_url = requests.get(
-                    "https://api.github.com/repos/MasoniteFramework/cookie-cutter/releases"
-                )
-                tags = []
-
-                for release in get_zip_url.json():
-                    if release["prerelease"] is False:
-                        tags.append(release["tag_name"].replace("v", ""))
-
-                tags = sorted(
-                    tags, key=lambda v: [int(i) for i in v.split(".")], reverse=True
-                )
-
-                get_zip_url = requests.get(
-                    "https://api.github.com/repos/MasoniteFramework/cookie-cutter/releases/tags/v{0}".format(
-                        tags[0]
-                    )
-                )
-
-                zipball = get_zip_url.json()["zipball_url"]
-        except TypeError:
-            raise ProjectLimitReached(
-                "You have reached your hourly limit of creating new projects. Try again in 1 hour."
+            tags = sorted(
+                tags, key=lambda v: [int(i) for i in v.split(".")], reverse=True
             )
+            # get url from latest tagged version
+            if not tags:
+                self.info("No tags has been found, using latest commit on master.")
+                zipball = self.get_branch_archive_url(provider, repo, "master")
+            else:
+                zipball = self.get_tag_archive_url(provider, repo, tags[0])
+        # except Exception as e:
+        #     import pdb; pdb.set_trace()
+        #     raise ProjectLimitReached(
+        #         "You have reached your hourly limit of creating new projects. Try again in 1 hour."
+        #     )
         success = False
 
         zipurl = zipball
@@ -185,3 +175,78 @@ class NewCommand(Command):
 
         else:
             self.comment("Could Not Create Application :(")
+
+    def set_api_provider_url_for_repo(self, provider, repo):
+        if provider == "github":
+            self.api_base_url =  "https://api.github.com/repos/{0}".format(repo)
+        elif provider == "gitlab":
+            import urllib.parse
+            repo_encoded_url =urllib.parse.quote(repo, safe="")
+            self.api_base_url = "https://gitlab.com/api/v4/projects/{0}".format(repo_encoded_url)
+
+    def get_branch_provider_data(self, provider, branch):
+        import requests
+        if provider == "github":
+            branch_data = requests.get(
+                "{0}/branches/{1}".format(
+                    self.self.api_base_url, branch
+                )
+            )
+        elif provider == "gitlab":
+            branch_data = requests.get(
+                "{0}/repository/branches/{1}".format(
+                    self.self.api_base_url, branch
+                )
+            )
+        return branch_data.json()
+
+    def get_branch_archive_url(self, provider, repo, branch):
+        if provider == "github":
+            return "https://github.com/{0}/archive/{1}.zip".format(repo, branch)
+        elif provider == "gitlab":
+            # here we can provide commit, branch name or tag
+            return "{0}/repository/archive.zip?sha={1}.zip".format(self.api_base_url, branch)
+
+    def get_tag_archive_url(self, provider, repo, version):
+        if provider == "github":
+            import requests
+            tag_data = requests.get(
+                "{0}/releases/tags/v{1}".format(
+                    self.api_base_url, version
+                )
+            )
+            return tag_data.json()["zipball_url"]
+        elif provider == "gitlab":
+            return self.get_branch_archive_url("gitlab", repo, "v" + version)
+
+    def get_releases_provider_data(self, provider):
+        import requests
+        if provider == "github":
+            releases_data = requests.get(
+                "{0}/releases".format(self.api_base_url)
+            )
+        elif provider == "gitlab":
+            releases_data = requests.get(
+                "{0}/releases".format(self.api_base_url)
+            )
+        return releases_data.json()
+
+    def get_release_archive_url_from_release_data(self, provider, release):
+        if provider == "github":
+            return release["zipball_url"]
+        elif provider == "gitlab":
+            return [x for x in release["assets"]["sources"] if x["format"] == "zip" ][0]["url"]
+            # could also do
+            # return "{0}/repository/archive.zip?sha={1}.zip".format(self.api_base_url, branch)
+
+    def get_tags_provider_data(self, provider):
+        import requests
+        if provider == "github":
+            releases_data = requests.get(
+                "{0}/releases".format(self.api_base_url)
+            )
+        elif provider == "gitlab":
+            releases_data = requests.get(
+                "{0}/repository/tags".format(self.api_base_url)
+            )
+        return releases_data.json()

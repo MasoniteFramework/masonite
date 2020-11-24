7fb3e21be3f9be4d99fe060cf9db286101e44526
Joe Mancuso <idmann509@gmail.com>
2020-11-24 18:47:21 -0500

    fixed new command

--- src/masonite/commands/NewCommand.py
+++ src/masonite/commands/NewCommand.py
@@ -21,8 +21,8 @@ class NewCommand(Command):
         {target? : Path of you Masonite project}
         {--b|--branch=False : Specify which branch from the Masonite repo you would like to install}
         {--r|--release=False : Specify which version of Masonite you would like to install}
-        {--rp|--repo=MasoniteFramework/cookie-cutter : Specify from which repository you want to craft your project}
-        {--p|--provider=github : Specify from which repository you want to craft your project [github, gitlab] }
+        {--repo=MasoniteFramework/cookie-cutter : Specify from which repository you want to craft your project}
+        {--p|--provider=github : Specify from which repository you want to craft your project github, gitlab }
     """
 
     providers = ["github", "gitlab"]

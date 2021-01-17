9aabb9864259f48a507f8e65b9c9e7a8ee223fdf
Joe Mancuso <idmann509@gmail.com>
2021-01-17 14:51:51 -0500

    fixed templates

--- src/masonite/__version__.py
+++ src/masonite/__version__.py
@@ -2,7 +2,7 @@
 __title__ = "masonite"
 __description__ = "The core for the Masonite framework"
 __url__ = "https://github.com/MasoniteFramework/masonite"
-__version__ = "3.0.2"
+__version__ = "3.0.3"
 __author__ = "Joseph Mancuso"
 __author_email__ = "joe@masoniteproject.com"
 __licence__ = "MIT"

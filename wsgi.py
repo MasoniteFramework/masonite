4cc34536db88cb92f6162b4d193a1b7760ffcc64
Joe Mancuso <idmann509@gmail.com>
2021-02-07 09:39:50 -0500

    fixed craft serve

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@
 
 from src.masonite.app import App
 
-from src.masonite.wsgi import response_handler
+from src.masonite.wsgi import response_handler, package_response_handler
 from src.masonite.helpers import config
 from src.masonite.environment import LoadEnvironment
 
@@ -15,7 +15,7 @@ LoadEnvironment()
 
 container = App()
 
-container.bind('WSGI', response_handler)
+container.bind('WSGI', package_response_handler)
 container.bind('Container', container)
 
 container.bind('Providers', [])

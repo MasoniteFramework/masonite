50eb33a7affe668a95c59b97b4de8e1ffc1c93d6
Joe Mancuso <idmann509@gmail.com>
2021-12-05 21:08:33 -0500

    fixed amqp driver

--- src/masonite/drivers/queue/QueueAmqpDriver.py
+++ src/masonite/drivers/queue/QueueAmqpDriver.py
@@ -71,11 +71,9 @@ class QueueAmqpDriver(BaseQueueDriver, QueueContract, HasColoredCommands):
             try:
                 self._publish(payload)
             except (
-                (
-                    self.pika.exceptions.ConnectionClosed,
-                    self.pika.exceptions.ChannelClosed,
-                ),
-                additional_exceptions,
+                self.pika.exceptions.ConnectionClosed,
+                self.pika.exceptions.ChannelClosed,
+                *additional_exceptions,
             ):
                 self.connect()
                 self._publish(payload)

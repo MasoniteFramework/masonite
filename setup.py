f3088e124c40d0f9cd9de8f4518dd3ae4c296db6
Joseph Mancuso <idmann509@gmail.com>
2021-12-30 09:48:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="4.0.2",
+    version="4.1.0",
     package_dir={"": "src"},
     description="The Masonite Framework",
     long_description=long_description,

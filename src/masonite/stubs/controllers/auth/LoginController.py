cc39b1e9e3c9721cd5f96e33b56959b5522ce7bc
Joe Mancuso <idmann509@gmail.com>
2022-01-01 16:53:09 -0500

    fixed logout

--- src/masonite/stubs/controllers/auth/LoginController.py
+++ src/masonite/stubs/controllers/auth/LoginController.py
@@ -9,7 +9,7 @@ class LoginController(Controller):
     def show(self, view: View):
         return view.render("auth.login")
 
-    def store(self, view: View, request: Request, auth: Auth, response: Response):
+    def store(self, request: Request, auth: Auth, response: Response):
         login = auth.attempt(request.input("username"), request.input("password"))
 
         if login:
@@ -19,3 +19,7 @@ class LoginController(Controller):
         return response.redirect(name="login").with_errors(
             ["The email or password is incorrect"]
         )
+
+    def logout(self, auth: Auth, response: Response):
+        auth.logout()
+        return response.redirect(name="login")

dfdd4769d1516bbe19938291ff42a9597562aeef
Joe Mancuso <idmann509@gmail.com>
2022-01-19 20:47:48 -0500

    added subdomain activation

--- src/masonite/providers/FrameworkProvider.py
+++ src/masonite/providers/FrameworkProvider.py
@@ -1,3 +1,4 @@
+from email.mime import application
 from ..foundation import response_handler
 from ..request import Request
 from ..response import Response
@@ -14,6 +15,8 @@ class FrameworkProvider:
     def boot(self):
         request = Request(self.application.make("environ"))
         request.app = self.application
+        if self.application.has('activate.subdomains') and self.application.make('activate.subdomains'):
+            request.activate_subdomains()
         self.application.bind("request", request)
         self.application.bind("response", Response(self.application))
         self.application.bind("start_time", time.time())

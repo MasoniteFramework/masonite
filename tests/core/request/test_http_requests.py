2b3c4dca6975cd3af579455e728619ac8e4ab989
Samuel Girardin <samuelgirardin@pm.me>
2022-01-21 15:26:12 +0100

    add unit test

--- tests/core/request/test_http_requests.py
+++ tests/core/request/test_http_requests.py
@@ -1,3 +1,6 @@
+import os
+from src.masonite.exceptions.exceptions import RouteNotFoundException
+
 from tests import TestCase
 
 
@@ -5,3 +8,10 @@ class TestHttpRequests(TestCase):
     def test_csrf_request(self):
         self.withoutCsrf()
         return self.post("/").assertContains("Welcome")
+
+    def test_route_not_found_throw_exception_in_dev_mode(self):
+        os.environ["APP_ENV"] = "development"
+        with self.assertRaises(RouteNotFoundException) as e:
+            self.get("/unknown/route")
+
+        self.assertEqual(str(e.exception), "GET /unknown/route : 404 Not Found")

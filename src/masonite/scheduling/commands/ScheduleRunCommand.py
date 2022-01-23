26238126812aaa1955b06a4f81693fb38f795105
Samuel Girardin <samuelgirardin@pm.me>
2022-01-23 12:49:03 +0100

    add back task name to run and add new force flag

--- src/masonite/scheduling/commands/ScheduleRunCommand.py
+++ src/masonite/scheduling/commands/ScheduleRunCommand.py
@@ -1,16 +1,13 @@
 """ A ScheduleRunCommand Command """
-import pendulum
-import inspect
 from cleo import Command
-
-from ..Task import Task
 
 
 class ScheduleRunCommand(Command):
     """
     Run the scheduled tasks
     schedule:run
-        {--t|task=None : Name of task you want to run}
+        {--t|task=? : Name of task you want to run (else all scheduled tasks will be ran)}
+        {--f|force : Force running task immediately}
     """
 
     def __init__(self, application):
@@ -18,4 +15,6 @@ class ScheduleRunCommand(Command):
         self.app = application
 
     def handle(self):
-        return self.app.make("scheduler").run()
+        return self.app.make("scheduler").run(
+            run_name=self.option("task"), force=self.option("force")
+        )

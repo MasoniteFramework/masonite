a3e26beba4f5e4ae952956fba1f302023144aa73
Samuel Girardin <samuelgirardin@pm.me>
2022-02-10 09:43:38 +0100

    fix rate provider

--- src/masonite/rates/providers/RateProvider.py
+++ src/masonite/rates/providers/RateProvider.py
@@ -9,3 +9,6 @@ class RateProvider(Provider):
     def register(self):
         rate_limiter = RateLimiter(self.application)
         self.application.bind("rate", rate_limiter)
+
+    def boot(self):
+        pass

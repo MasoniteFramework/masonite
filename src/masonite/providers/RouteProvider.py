dd5a39d7242c68e9922feef1857db55545fa3a19
Samuel Girardin <samuelgirardin@pm.me>
2022-02-10 09:43:06 +0100

    add rate provider

--- src/masonite/providers/RouteProvider.py
+++ src/masonite/providers/RouteProvider.py
@@ -1,5 +1,4 @@
 from inspect import isclass
-import pdb
 
 from ..response import Response
 from ..facades import Response as ResponseFacade
@@ -8,7 +7,6 @@ from ..routes import Route
 from ..routes.commands import RouteListCommand
 from ..pipeline import Pipeline
 from ..exceptions import RouteNotFoundException
-from ..rates import RateLimiter
 
 
 class RouteProvider(Provider):
@@ -18,9 +16,6 @@ class RouteProvider(Provider):
     def register(self):
         Route.set_controller_locations(self.application.make("controllers.location"))
         self.application.make("commands").add(RouteListCommand(self.application))
-        # register rate limiter
-        rate_limiter = RateLimiter(self.application)
-        self.application.bind("rate", rate_limiter)
 
     def boot(self):
         router = self.application.make("router")
--- tests/integrations/providers/AppProvider.py
+++ tests/integrations/providers/AppProvider.py
@@ -1,6 +1,6 @@
 from src.masonite.providers import Provider
 from src.masonite.facades import Gate, RateLimiter
-from src.masonite.rates import GlobalRate, LimitedForGuestsRate
+from src.masonite.rates import GuestsOnlyLimiter
 
 
 class AppProvider(Provider):
@@ -13,7 +13,7 @@ class AppProvider(Provider):
         Gate.define("display-admin", lambda user: user.email == "admin@gmail.com")
 
         # Register rate limits for api
-        RateLimiter.register("api", LimitedForGuestsRate("5/hour"))
+        RateLimiter.register("api", GuestsOnlyLimiter("5/hour"))
 
     def boot(self):
         pass

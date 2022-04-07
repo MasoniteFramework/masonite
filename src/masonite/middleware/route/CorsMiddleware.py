c6dc437850e7e12a32df0d03623cb59fc1c0aa5a
Samuel Girardin <samuelgirardin@pm.me>
2022-04-07 18:11:57 +0200

    use cors service in middleware

--- src/masonite/middleware/route/CorsMiddleware.py
+++ src/masonite/middleware/route/CorsMiddleware.py
@@ -1,12 +1,45 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ...request import Request
+    from ...response import Response
+
+from ...cors import Cors
+
+
 class CorsMiddleware:
+    def before(self, request: "Request", response: "Response", cors: Cors):
 
-    headers = {}
+        if not self.match_paths(request):
+            return request
 
-    def before(self, request, response):
+        if cors.is_preflight(request):
+            response = cors.build_preflight_response(request, response)
+            response = cors.update_vary_header(
+                response, "Access-Control-Request-Method"
+            )
+            return response
+
+        return request
+
+    def after(self, request: "Request", response: "Response", cors: Cors):
         if request.get_request_method() == "OPTIONS":
-            return response.view("preflight")
-        return request
+            cors.update_vary_header(response, "Access-Control-Request-Method")
 
-    def after(self, request, response):
-        response.header(self.headers)
-        return request
+        response = cors.add_actual_request_headers(request)
+        return response
+
+    def match_paths(self, request: "Request", cors):
+        host = request.get_host()
+        request_path = request.get_path()
+        paths = cors.options.get("paths")
+
+        for path in paths:
+            if path != "/":
+                path = path.strip("/")
+            if path == host:
+                return True
+            elif request_path == path:
+                return True
+
+        return False

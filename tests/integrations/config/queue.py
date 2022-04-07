ec783404c1cfce76d9a04b86bfac3353fa73a2b3
Samuel Girardin <samuelgirardin@pm.me>
2022-04-07 16:43:12 +0200

    reorder amqp options

--- tests/integrations/config/queue.py
+++ tests/integrations/config/queue.py
@@ -11,20 +11,19 @@ DRIVERS = {
     "redis": {
         #
     },
-    # See https://pika.readthedocs.io/en/stable/modules/parameters.html#pika.connection.URLParameters
-    # for valid connection options values
     "amqp": {
         "username": "guest",
         "password": "guest",
         "port": "5672",
         "vhost": "",
         "host": "localhost",
-        "exchange": "",
+        # See https://pika.readthedocs.io/en/stable/modules/parameters.html#pika.connection.URLParameters
+        # for valid connection options values
         "connection_options": {},
+        "exchange": "",
         "channel": "default",
         "queue": "masonite4",
         "tz": "UTC",
-        "exchange": "",
     },
     "async": {
         "blocking": True,

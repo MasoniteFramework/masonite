df43bf91fe58bf3e4f440b6a9817d7f0d4f98971
Samuel Girardin <samuelgirardin@pm.me>
2022-04-25 10:21:03 +0200

    revert PR to original code

--- src/masonite/middleware/route/SessionMiddleware.py
+++ src/masonite/middleware/route/SessionMiddleware.py
@@ -1,4 +1,3 @@
-from src.masonite.validation import MessageBag
 from .. import Middleware
 from ...utils.str import random_string
 from ...facades import Request, Session, Response
@@ -28,10 +27,6 @@ class SessionMiddleware(Middleware):
     def with_errors(self, errors):
         if isinstance(errors, list):
             Session.flash("errors", {"errors": errors})
-        # we could do this but don't want to as we want to keep MessageBag instance to ease retrieving
-        # error message in views
-        # elif isinstance(errors, MessageBag):
-        #     Session.flash("errors", errors.all())
         else:
             Session.flash("errors", errors)
         return Response

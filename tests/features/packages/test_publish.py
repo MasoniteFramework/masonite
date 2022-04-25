f66ecae954dbb074e8c9c75e096fedeca9d631a6
Samuel Girardin <samuelgirardin@pm.me>
2022-04-25 15:14:40 +0200

    simplify test feature

--- tests/features/packages/test_publish.py
+++ tests/features/packages/test_publish.py
@@ -1,26 +1,10 @@
-import os
 from tests import TestCase
-from src.masonite.utils.location import migrations_path
 
 
 class TestPackageProviderPublishing(TestCase):
     def test_publishing_migrations(self):
-        timestamp = self.fakeTime().format("YYYY_MM_DD_HHmmss")
         self.craft(
-            "package:publish", "test_package --r migrations"
+            "package:publish", "test_package --r migrations --dry"
         ).assertOutputContains("1_create_some_table.py").assertOutputContains(
             "2_create_other_table.py"
         )
-
-        published_migrations = []
-        dump(migrations_path())
-        for migration in os.listdir(migrations_path()):
-            if migration.startswith(timestamp):
-                published_migrations.append(migration)
-
-        self.assertIn(f"{timestamp}_1_create_some_table.py", published_migrations)
-        self.assertIn(f"{timestamp}_2_create_other_table.py", published_migrations)
-
-        # clean up files after publishing test migrations
-        for migration in published_migrations:
-            os.remove(migrations_path(migration))

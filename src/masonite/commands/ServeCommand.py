cc892a7fdce17c0ca7f2f8ad2f4a4a32c0304ed0
Samuel Girardin <samuelgirardin@pm.me>
2022-04-28 10:57:17 +0200

    switch to simple server from wsgiref standard python module

--- src/masonite/commands/ServeCommand.py
+++ src/masonite/commands/ServeCommand.py
@@ -1,8 +1,51 @@
 import sys
-
 import hupper
-import waitress
+import logging
+import socketserver
+from wsgiref import simple_server
+
 from .Command import Command
+
+logger = logging.getLogger("masonite.server")
+
+
+class ThreadedWSGIServer(socketserver.ThreadingMixIn, simple_server.WSGIServer):
+    """A threaded version of the WSGIServer"""
+
+    daemon_threads = True
+
+
+class WSGIRequestHandler(simple_server.WSGIRequestHandler):
+    def log_message(self, format, *args):
+        extra = {
+            "request": self.request,
+            "server_time": self.log_date_time_string(),
+        }
+        if args[1][0] == "4":
+            # 0x16 = Handshake, 0x03 = SSL 3.0 or TLS 1.x
+            if args[0].startswith("\x16\x03"):
+                extra["status_code"] = 500
+                logger.error(
+                    "You're accessing the development server over HTTPS, but "
+                    "it only supports HTTP.",
+                    extra=extra,
+                )
+                return
+
+        if args[1].isdigit() and len(args[1]) == 3:
+            status_code = int(args[1])
+            extra["status_code"] = status_code
+
+            if status_code >= 500:
+                level = logger.error
+            elif status_code >= 400:
+                level = logger.warning
+            else:
+                level = logger.info
+        else:
+            level = logger.info
+
+        level(format, *args, extra=extra)
 
 
 class ServeCommand(Command):
@@ -16,6 +59,7 @@ class ServeCommand(Command):
         {--d|dont-reload : Make the server NOT automatically reload on file changes}
         {--i|reload-interval=1 : Number of seconds to wait to reload after changed are detected}
         {--l|live-reload : Make the server automatically refresh your web browser}
+        {--t|threaded : Use a multi-threaded development server}
     """
 
     def __init__(self, application):
@@ -63,6 +107,7 @@ def main(args=sys.argv[1:]):
 
     host = "127.0.0.1"
     port = "8000"
+    threaded = False
     if "--host" in args:
         host = args[args.index("--host") + 1]
     if "-b" in args:
@@ -71,9 +116,17 @@ def main(args=sys.argv[1:]):
         port = args[args.index("--port") + 1]
     if "-p" in args:
         port = args[args.index("-p") + 1]
+    if "--threading" in args or "-t" in args:
+        threaded = True
 
-    print(f"Serving on : http://{host}:{port}")
+    if threaded:
+        server_class = ThreadedWSGIServer
+    else:
+        server_class = simple_server.WSGIServer
 
-    waitress.serve(
-        application, host=host, port=port, clear_untrusted_proxy_headers=False
-    )
+    with simple_server.make_server(
+        host, int(port), application, server_class, WSGIRequestHandler
+    ) as httpd:
+        # Respond to requests until process is killed
+        print(f"Serving on : http://{host}:{port}")
+        httpd.serve_forever()

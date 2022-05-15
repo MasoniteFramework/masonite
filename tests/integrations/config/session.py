ee965791b0f42f9fa6803fb1be288d557329cf56
Kieren Eaton <circuloned@gmail.com>
2022-05-15 08:23:19 +0800

    Added Redis Session Driver test

--- tests/integrations/config/session.py
+++ tests/integrations/config/session.py
@@ -4,4 +4,13 @@ import os
 DRIVERS = {
     "default": "cookie",
     "cookie": {},
+    "redis": {
+        "driver_config": {
+            "host": "127.0.0.1",
+            "port": "6379",
+            "password": "",
+        },
+        "timeout": 60*60,
+        "namespace": "masonite4",
+    },
 }

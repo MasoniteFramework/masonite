3bd2c6714d75effea8c0c10ea929a69d497d96ea
Kieren Eaton <circuloned@gmail.com>
2022-05-18 19:55:51 +0800

    Dont rely on “default” driver as it may change

--- tests/features/session/test_session.py
+++ tests/features/session/test_session.py
@@ -5,7 +5,7 @@ from tests import TestCase
 class TestSession(TestCase):
     def test_default_sessiondriver(self):
         session = self.application.make("session")
-        session.start()
+        session.start("cookie")
         driver = session.get_driver()
         self.assertIsInstance(driver, CookieDriver)
 

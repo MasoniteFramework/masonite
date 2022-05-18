a8f6815024921af480f93623b3980e0a5da78147
Kieren Eaton <circuloned@gmail.com>
2022-05-18 20:03:19 +0800

    Adjusted test config

--- tests/integrations/config/session.py
+++ tests/integrations/config/session.py
@@ -5,11 +5,7 @@ DRIVERS = {
     "default": "cookie",
     "cookie": {},
     "redis": {
-        "driver_config": {
-            "host": "127.0.0.1",
-            "port": "6379",
-            "password": "",
-        },
+        "host": "127.0.0.1",
         "timeout": 60*60,
         "namespace": "masonite4",
     },

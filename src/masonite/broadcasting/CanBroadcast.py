3b8bba6b8449ca1e3bd52f3edf8526412dff885d
Samuel Girardin <samuelgirardin@pm.me>
2022-05-20 10:34:07 +0200

    type hint broadcast feature

--- src/masonite/broadcasting/CanBroadcast.py
+++ src/masonite/broadcasting/CanBroadcast.py
@@ -1,9 +1,15 @@
+from typing import Any
+
+
 class CanBroadcast:
-    def broadcast_on(self):
+    def broadcast_on(self) -> "str|list":
+        """Channel or list of channels to broadcast on."""
         return None
 
-    def broadcast_with(self):
+    def broadcast_with(self) -> Any:
+        """Data to broadcast."""
         return vars(self)
 
-    def broadcast_as(self):
+    def broadcast_as(self) -> str:
+        """Name of the event to broadcast as."""
         return self.__class__.__name__

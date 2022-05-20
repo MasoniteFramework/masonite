a381e89806155c7a1fb39f2e95a9cff138718ae8
Samuel Girardin <samuelgirardin@pm.me>
2022-05-20 11:28:16 +0200

    type hint cookies classes

--- src/masonite/cookies/CookieJar.py
+++ src/masonite/cookies/CookieJar.py
@@ -1,38 +1,49 @@
+from typing import Any, List, Tuple
 import pendulum
-from .Cookie import Cookie
 
+from .Cookie import Cookie
 from ..utils.time import cookie_expire_time
 
 
 class CookieJar:
-    def __init__(self):
-        self.cookies = {}
-        self.loaded_cookies = {}
-        self.deleted_cookies = {}
+    """Cookie jar used to manage cookies in the request and response."""
 
-    def add(self, name, value, **options):
+    def __init__(self):
+        self.cookies: dict = {}
+        self.loaded_cookies: dict = {}
+        self.deleted_cookies: dict = {}
+
+    def add(self, name: str, value: Any, **options) -> None:
+        """Create a cookie and add it to the jar."""
         self.cookies.update({name: Cookie(name, value, **options)})
 
-    def all(self):
+    def all(self) -> dict:
+        """Get all cookies (added manually and loaded from request) from the jar."""
         cookies = self.loaded_cookies
         cookies.update(self.cookies)
         return cookies
 
-    def all_added(self):
+    def all_added(self) -> dict:
+        """Get all cookies added to the jar."""
         return self.cookies
 
-    def get(self, name):
+    def get(self, name: str) -> "Cookie":
+        """Get a cookie with the given name."""
         aggregate = self.all()
         return aggregate.get(name)
 
-    def exists(self, name):
+    def exists(self, name: str) -> bool:
+        """Check if the given cookie name exists in the jar."""
         return name in self.cookies or name in self.loaded_cookies
 
-    def is_expired(self, name):
+    def is_expired(self, name: str) -> bool:
+        """Check if the given cookie name is expired."""
         cookie = self.get(name)
         return cookie.expires < pendulum.now()
 
-    def delete(self, name):
+    def delete(self, name: str) -> None:
+        """Delete a cookie with the given name from the jar. This will remove it from the jar
+        and flag it as a cookie to be deleted (by forcing its expiration date)."""
         self.deleted_cookies.update(
             {
                 name: Cookie(
@@ -46,10 +57,12 @@ class CookieJar:
         if name in self.loaded_cookies:
             self.loaded_cookies.pop(name)
 
-    def load_cookie(self, key, value):
-        self.loaded_cookies.update({key: Cookie(key, value)})
+    def load_cookie(self, name: str, value: Any) -> None:
+        """Load a cookie into the jar with given name and value."""
+        self.loaded_cookies.update({name: Cookie(name, value)})
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
+        """Render the jar as a dictionary containing all cookies added manually and loaded cookies."""
         dic = {}
         aggregate = self.cookies
         aggregate.update(self.loaded_cookies)
@@ -58,16 +71,25 @@ class CookieJar:
 
         return dic
 
-    def load(self, cookie_string):
+    def load(self, cookie_string: str) -> "CookieJar":
+        """Load a cookie into the jar from a HTTP cookie string."""
         for compound_value in cookie_string.split("; "):
             if "=" in compound_value:
                 key, value = compound_value.split("=", 1)
                 self.load_cookie(key, value)
         return self
 
-    def render_response(self):
+    def render_response(self) -> "List[Tuple[str, str]]":
+        """Render the cookie jar as list of tuple representing each cookies. This will be used to
+        be inserted in the WSGI response.
+
+        [
+            ("Set-Cookie", "name=value; Expires=Thu, 31 Oct 2021 07:28:00 GMT;"),
+            ("Set-Cookie", "other_name=value2;")
+        ]
+        """
         cookies = []
-        for name, cookie in {**self.deleted_cookies, **self.all_added()}.items():
+        for cookie in {**self.deleted_cookies, **self.all_added()}.values():
             cookies.append(("Set-Cookie", cookie.render()))
 
         return cookies

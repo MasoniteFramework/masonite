a697ce72121224a88ddc17901e4426e22447ddfd
Kieren Eaton <circuloned@gmail.com>
2022-05-22 18:10:42 +0800

    Readded port and password options for consistency

--- tests/integrations/config/session.py
+++ tests/integrations/config/session.py
@@ -6,7 +6,9 @@ DRIVERS = {
     "cookie": {},
     "redis": {
         "host": "127.0.0.1",
-        "options": {"db": 1},
+        "port": 6379,
+        "password": "",
+        "options": {"db": 1}, # redis module driver specific options
         "timeout": 60*60,
         "namespace": "masonite4",
     },

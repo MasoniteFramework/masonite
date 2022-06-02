c992029ee97b1aa49d4c2e2e8c3029541aba7dc6
Kieren Eaton <circuloned@gmail.com>
2022-06-03 05:34:18 +0800

    Added mergedeep dependency to setup.py

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "pytest>=7<8",
         "werkzeug>=2<3",
         "watchdog>=2<3",
+        "mergedeep>1,<2",
     ],
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[

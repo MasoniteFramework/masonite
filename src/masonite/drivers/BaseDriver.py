22ae4c5d323c6d26968f2b3af9c5bedd500d1bcc
Kieren Eaton <circuloned@gmail.com>
2022-06-02 09:34:14 +0800

    Fixed  default options not passed to connection
    separated default options into a separate var for clarity
    use mergedeep to combine nested dicts

--- src/masonite/drivers/BaseDriver.py
+++ src/masonite/drivers/BaseDriver.py
@@ -1,12 +1,14 @@
+from mergedeep import merge
 
 class BaseDriver:
     """Base Session driver"""
 
     def __init__(self, application: "Application"):
         self.application = application
+        self.default_options = {}
         self.options = {}
 
-    def set_options(self, options):
-        # allow internal driver defaults
-        self.options.update(options)
+    def set_options(self, options: dict):
+        # merge options with defaults
+        self.options = merge(self.default_options, self.options, options)
         return self

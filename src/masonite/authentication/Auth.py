ab44efa869147289f53cefcd4f08ab34eac5b6aa
Samuel Girardin <samuelgirardin@pm.me>
2022-06-02 14:12:52 +0200

    add type hinting auth

--- src/masonite/authentication/Auth.py
+++ src/masonite/authentication/Auth.py
@@ -1,36 +1,43 @@
 import pendulum
 import uuid
+from typing import TYPE_CHECKING, Any, List, Tuple
+
 from ..routes import Route
+
+if TYPE_CHECKING:
+    from ..foundation import Application
 
 
 class Auth:
-    def __init__(self, application, guard_config=None):
+    def __init__(self, application: "Application", guard_config: dict = {}):
         self.application = application
-        self.guards = {}
+        self.guards: dict = {}
         self._guard = None
-        self.guard_config = guard_config or {}
-        self.options = {}
-
+        self.guard_config: dict = guard_config or {}
+        self.options: dict = {}
         self._user = None
 
-    def add_guard(self, name, guard):
+    def add_guard(self, name: str, guard: "Any") -> None:
+        """Register a new authentication guard."""
         self.guards.update({name: guard})
 
-    def set_configuration(self, config):
+    def set_configuration(self, config: dict) -> "Auth":
         self.guard_config = config
         return self
 
-    def guard(self, guard):
+    def guard(self, guard: "Any") -> "Auth":
+        """Set the current authentication guard to use."""
         self._guard = guard
         return self
 
-    def get_guard(self, name=None):
+    def get_guard(self, name: str = None) -> "Any":
+        """Get the default authentication guard or the guard with given name."""
         if name is None and self._guard is None:
             return self.guards[self.guard_config.get("default")]
 
         return self.guards[self._guard]
 
-    def get_config_options(self, guard=None):
+    def get_config_options(self, guard: str = None) -> dict:
         if guard is None:
             options = self.guard_config.get(self.guard_config.get("default"), {})
             options.update(self.options)
@@ -40,60 +47,47 @@ class Auth:
         options.update(self.options)
         return options
 
-    def attempt(self, email, password, once=False):
+    def attempt(self, email: str, password: str, once: bool = False):
+        """Attempt to authenticate the user with the given email/password."""
         auth_config = self.get_config_options()
         auth_config.update({"once": once})
         user = self.get_guard().set_options(auth_config).attempt(email, password)
         self.set_user(user)
         return user
 
-    def attempt_by_id(self, user_id, once=False):
+    def attempt_by_id(self, user_id: "str|int", once: bool = False):
+        """Attempt to authenticate the user with the given user ID."""
         auth_config = self.get_config_options()
         auth_config.update({"once": once})
         user = self.get_guard().set_options(auth_config).attempt_by_id(user_id)
         self.set_user(user)
         return user
 
-    def logout(self):
-        """Logout the current authenticated user.
-
-        Returns:
-            self
-        """
+    def logout(self) -> None:
+        """Logout the current authenticated user."""
         self.application.make("request").remove_user()
         self.application.make("response").delete_cookie("token")
         self.get_guard().set_options(self.get_config_options()).logout()
         self._user = None
 
-    def user(self):
-        """Get the current authenticated user.
-
-        Returns:
-            self
-        """
+    def user(self) -> "Any":
+        """Get the current authenticated user."""
         if self._user:
             return self._user
         return self.get_guard().set_options(self.get_config_options()).user()
 
-    def register(self, dictionary):
-        """Logout the current authenticated user.
-
-        Returns:
-            self
-        """
+    def register(self, dictionary: dict) -> "Any":
+        """Register a new user with given data."""
         auth_config = self.get_config_options()
         return self.get_guard().set_options(auth_config).register(dictionary)
 
-    def set_user(self, user):
+    def set_user(self, user: "Any") -> "Auth":
+        """Set the current authenticated user."""
         self._user = user
         return self
 
-    def password_reset(self, email):
-        """Logout the current authenticated user.
-
-        Returns:
-            self
-        """
+    def password_reset(self, email: str) -> "Tuple[None, None]|Tuple[int,str]":
+        """Reset password of the user with the given email."""
         token = str(uuid.uuid4())
         auth_config = self.get_config_options()
         auth = self.get_guard().set_options(auth_config).get_auth_column(email)
@@ -130,13 +124,8 @@ class Auth:
         self.application.make("event").fire("auth.password_reset", email, token)
         return auth, token
 
-    def reset_password(self, password, token):
-        """Logout the current authenticated user.
-
-        Returns:
-            self
-        """
-
+    def reset_password(self, password: str, token: str) -> bool:
+        """Reset password of the user with the given authentication token."""
         reset_record = (
             self.application.make("builder")
             .new()
@@ -163,7 +152,8 @@ class Auth:
         return True
 
     @classmethod
-    def routes(self):
+    def routes(self) -> "List[Route]":
+        """Get the basic authentication routes."""
         return [
             Route.get("/login", "auth.LoginController@show").name("login"),
             Route.get("/logout", "auth.LoginController@logout").name("logout"),
--- src/masonite/authentication/guards/WebGuard.py
+++ src/masonite/authentication/guards/WebGuard.py
@@ -1,35 +1,41 @@
-class WebGuard:
-    def __init__(self, application):
-        self.application = application
-        self.connection = None
+from typing import TYPE_CHECKING, Any, Tuple, List
 
-    def set_options(self, options):
+if TYPE_CHECKING:
+    from ...foundation import Application
+
+
+class WebGuard:
+    """Web authentication guard to use in classic HTTP requests."""
+
+    def __init__(self, application: "Application"):
+        self.application = application
+
+    def set_options(self, options: dict) -> "WebGuard":
         self.options = options
         return self
 
-    def attempt(self, username, password):
+    def attempt(self, username: str, password: str) -> "Any":
+        """Attempt to authenticate the user with the given username/password."""
         attempt = self.options.get("model")().attempt(username, password)
         if attempt and not self.options.get("once"):
             self.application.make("response").cookie("token", attempt.remember_token)
             self.application.make("request").set_user(attempt)
             return attempt
 
-    def get_auth_column(self, username):
+    def get_auth_column(self, username: str) -> str:
+        """Get the authentication column."""
         return self.options.get("model")().get_auth_column(username)
 
-    def register(self, dictionary):
+    def register(self, dictionary: dict) -> "Any":
+        """Register a new user with given data."""
         try:
             register = self.options.get("model")().register(dictionary)
         except Exception:
             return False
         return self.attempt_by_id(register.get_id())
 
-    def user(self):
-        """Get the currently logged in user.
-
-        Returns:
-            object|bool -- Returns the current authenticated user object or False or None if there is none.
-        """
+    def user(self) -> "Any|bool":
+        """Get the currently logged in user."""
         token = self.application.make("request").cookie("token")
         if token and self.options.get("model")():
             return (
@@ -39,15 +45,8 @@ class WebGuard:
 
         return False
 
-    def attempt_by_id(self, user_id):
-        """Login a user by the user ID.
-
-        Arguments:
-            user_id {string|int} -- The ID of the user model record.
-
-        Returns:
-            object|False -- Returns the current authenticated user object or False or None if there is none.
-        """
+    def attempt_by_id(self, user_id: "str|int"):
+        """Attempt to authenticate the user with the given user ID."""
         attempt = self.options.get("model")().attempt_by_id(user_id)
 
         if attempt and not self.options.get("once"):
@@ -57,15 +56,8 @@ class WebGuard:
 
         return False
 
-    def reset_password(self, username, new_password):
-        """Login a user by the user ID.
-
-        Arguments:
-            user_id {string|int} -- The ID of the user model record.
-
-        Returns:
-            object|False -- Returns the current authenticated user object or False or None if there is none.
-        """
+    def reset_password(self, username: str, new_password: str):
+        """Change password of the user with the given username and the new password."""
         attempt = self.options.get("model")().reset_password(username, new_password)
 
         if attempt:

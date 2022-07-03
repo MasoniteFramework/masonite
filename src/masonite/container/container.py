7cecdd22df87f3155228b550fd5160a523643b40
netbounty <filip.petrovic@netbounty.de>
2022-07-03 18:13:36 +0200

    added comment for singleton method in container.py

--- src/masonite/container/container.py
+++ src/masonite/container/container.py
@@ -88,6 +88,12 @@ class Container:
         return self
 
     def singleton(self, name, class_obj):
+        """Register a shared binding in the container.
+        
+        Arguments:
+            name {string} -- Name of the key you want to bind the object to
+            class_obj {object} -- The object you want to bind
+        """
         obj = self.resolve(class_obj)
         self.bind(name, obj)
 

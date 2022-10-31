0f9a102839182a5778b1fb6f20375107550f3948
Samuel Girardin <samuelgirardin@pm.me>
2022-10-31 19:07:35 +0100

    mark Python 3.11 as supported by the framework

--- setup.py
+++ setup.py
@@ -75,11 +75,11 @@ setup(
         "Operating System :: OS Independent",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     # What does your project relate to?

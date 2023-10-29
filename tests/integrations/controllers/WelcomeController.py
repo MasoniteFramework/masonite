a1c92a954aff89d0c534b83667b2c09b70d37955
Joe Mancuso <idmann509@gmail.com>
2023-10-28 21:02:17 -0400

    removed duplicate code

--- tests/integrations/controllers/WelcomeController.py
+++ tests/integrations/controllers/WelcomeController.py
@@ -176,9 +176,6 @@ class WelcomeController(Controller):
     def any(self, request: Request):
         return "any"
 
-    def fallback(self, request: Request):
-        return "fallback"
-
     def dd(self, request: Request):
         dump({"test": "value"})
         dd(request)

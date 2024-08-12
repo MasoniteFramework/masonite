612e1cbbf4f50b180eb4998cd69f8ecaa1a445c2
Kieren Eaton <499977+circulon@users.noreply.github.com>
2024-08-12 19:12:30 +0800

    Added opt-out for cookie encryption

--- tests/core/middleware/test_encrypt_cookies.py
+++ tests/core/middleware/test_encrypt_cookies.py
@@ -15,3 +15,16 @@ class TestEncryptCookiesMiddleware(TestCase):
         response.cookie("test", "value")
         EncryptCookies().after(request, response)
         self.assertNotEqual(response.cookie("test"), "value")
+
+def test_encrypt_cookies_opt_out(self):
+        request = self.make_request(
+            {"HTTP_COOKIE": f"test_key=test value"}
+        )
+
+        response = self.make_response()
+        EncryptCookies().before(request, None)
+        self.assertEqual(request.cookie("test_key", encrypt=False), "test value")
+
+        response.cookie("test", "value")
+        EncryptCookies().after(request, response)
+        self.assertNotEqual(response.cookie("test_key", encrypt=False), "test value")

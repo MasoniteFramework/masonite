104300ca910f9d62df4f830dbe360a78282fa390
Joe Mancuso <idmann509@gmail.com>
2024-10-16 16:10:51 -0400

    Refactor old() helper to include a default option

--- src/masonite/sessions/helpers.py
+++ src/masonite/sessions/helpers.py
@@ -1,6 +1,6 @@
 from ..facades import Session
 
 
-def old(key: str) -> str:
-    """Helper used to get value from session for the given key."""
-    return Session.get(key) or ""
+def old(key: str, default: str = "") -> str:
+    """Helper used to get value from session for the given key, with a default option."""
+    return Session.get(key) or default

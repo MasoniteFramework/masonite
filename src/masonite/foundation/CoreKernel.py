c7e6b6b96ac2e3758826cf00860afe4b2609798a
Kieren Eaton <499977+circulon@users.noreply.github.com>
2026-05-20 11:18:48 +0800

    Fixed CoreKernel could not be used as primary Kernel
    Fixed allowing app (Kernel) registration to use the CoreKernel
    fixed possible issue where setup could be borked if CoreKernel was subclassed

--- src/masonite/foundation/CoreKernel.py
+++ src/masonite/foundation/CoreKernel.py
@@ -1,4 +1,3 @@
-import os
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -8,43 +7,40 @@ if TYPE_CHECKING:
 class CoreKernel:
     def __init__(self, app: "Application"):
         self.application = app
-        self.register_core()
+        self._register_core()
 
-    def register_core(self) -> None:
+    def register(self) -> None:
+        pass
+
+    def _register_core(self) -> None:
         """Register core Masonite features in the project."""
-        self.load_environment()
-        self.register_framework()
-        self.register_command_capsule()
+        self._register_framework()
+        self._register_capsules()
 
-    def load_environment(self) -> None:
-        """Load environment variables into the application."""
-        from ..environment import LoadEnvironment
-        LoadEnvironment()
-
-    def register_framework(self) -> None:
-        """Register Core Fraamework requirements"""
+    def _register_framework(self) -> None:
+        """Register Core Framework requirements"""
+        import os
         from .response_handler import response_handler
-        from ..middleware import MiddlewareCapsule
-        from ..routes import Router
-        from ..loader import Loader
+        from ..environment import LoadEnvironment
 
+        LoadEnvironment()
         self.application.set_response_handler(response_handler)
         self.application.use_storage_path(
             os.path.join(self.application.base_path, "storage")
         )
-        self.application.bind("middleware", MiddlewareCapsule())
-        self.application.bind(
-            "router",
-            Router(),
-        )
-        self.application.bind("loader", Loader())
 
-    def register_command_capsule(self) -> None:
-        """Register the 'commands' binding in the application."""
+    def _register_capsules(self) -> None:
+        """Register the minimum required capsules."""
         from cleo import Application as CommandApplication
-        from ..commands import CommandCapsule
         from .. import __version__
+        from ..commands.CommandCapsule import CommandCapsule
+        from ..middleware import MiddlewareCapsule
+        from ..loader import Loader
+        from ..routes import Router
 
+        self.application.bind("middleware", MiddlewareCapsule())
+        self.application.bind("loader", Loader())
+        self.application.bind("router", Router())
         self.application.bind(
             "commands",
             CommandCapsule(CommandApplication("Masonite", __version__))
